a890bb43174467f73713477b747ded68a015f17f
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2024-04-18 13:18:03 -0700

    actually add install-awscli.sh
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

---
+++ .ci/install-awscli.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+set -o errexit -o verbose
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf ./aws
