fa6e6f2018a51149a1bcfa467e41462b0e398f1b
Brian L. Troutwine <brian.troutwine@datadoghq.com>
2024-04-22 06:25:33 -0700

    Introduce Regression Detector into the project (#14)
    Signed-off-by: Brian L. Troutwine <brian.troutwine@datadoghq.com>

---
+++ test/smp/configure-smp-aws-credentials.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Make sure we have the AWS CLI installed and that our named profile has been specified.
+command -v aws >/dev/null 2>&1 || { echo "AWS CLI (\`aws\`) must be installed and available on \$PATH" >&2; exit 1; }
+if [[ -z "${AWS_NAMED_PROFILE}" ]]; then
+    echo "\$AWS_NAMED_PROFILE must be present"
+    exit 1
+fi
+
+aws configure set aws_access_key_id $(aws ssm get-parameter --region us-east-1 --name ci.saluki.smp-bot-access-key-id --with-decryption --query "Parameter.Value" --out text) --profile ${AWS_NAMED_PROFILE}
+aws configure set aws_secret_access_key $(aws ssm get-parameter --region us-east-1 --name ci.saluki.smp-bot-access-key --with-decryption --query "Parameter.Value" --out text) --profile ${AWS_NAMED_PROFILE}
+aws configure set region us-west-2 --profile ${AWS_NAMED_PROFILE}
