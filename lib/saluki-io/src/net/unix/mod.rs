14b257eecc7c68eec2f74b826a9a1302e1ca3b6e
Toby Lawrence <toby@nuclearfurnace.com>
2024-04-22 17:27:13 +0000

    split recvmsg code further between linux/non-linux, and add cross to build CI image

--- lib/saluki-io/src/net/unix/mod.rs
+++ lib/saluki-io/src/net/unix/mod.rs
@@ -1,6 +1,8 @@
 use std::{io, os::unix::fs::FileTypeExt as _, path::Path};
 
+use bytes::BufMut;
 use socket2::SockRef;
+use tokio::net::{UnixDatagram, UnixStream};
 use tracing::trace;
 
 mod ancillary;
@@ -8,15 +10,14 @@ mod ancillary;
 #[cfg(target_os = "linux")]
 mod linux;
 #[cfg(target_os = "linux")]
-use self::linux::set_uds_passcred;
-
-mod recvmsg;
-pub(super) use self::recvmsg::{unix_recvmsg, unixgram_recvmsg};
+use self::linux::{set_uds_passcred, uds_recvmsg};
 
 #[cfg(not(target_os = "linux"))]
 mod non_linux;
 #[cfg(not(target_os = "linux"))]
-use self::non_linux::set_uds_passcred;
+use self::non_linux::{set_uds_passcred, uds_recvmsg};
+
+use super::addr::ConnectionAddress;
 
 /// Configures a Unix domain socket for use.
 ///
@@ -64,4 +65,29 @@ pub(super) async fn ensure_unix_socket_free<P: AsRef<Path>>(path: P) -> io::Resu
     }
 
     Ok(())
+}
+
+pub async fn unix_recvmsg<B: BufMut>(socket: &mut UnixStream, buf: &mut B) -> io::Result<(usize, ConnectionAddress)> {
+    // TODO: We technically don't need to do this for SOCK_STREAM because we can do it once when the
+    // connection is accepted, and then just do "normal" reads after that. We do still need to do it
+    // for SOCK_DGRAM, though... so we might want to actually consider updating our `socket2` PR to
+    // include support for even setting SO_PASSCRED (or the OS-specific equivalent) to also include
+    // macOS and FreeBSD (*BSD, really) so that this also works correctly for all
+    // `cfg(unix)`-capable platforms.
+
+    // We manually call `recvmsg` on our domain socket as stdlib/`mio`/`tokio` don't yet expose a way to do out-of-band
+    // reads to get ancillary data such as the socket credentials used to shuttle origin detection information.
+    socket
+        .async_io(tokio::io::Interest::READABLE, || uds_recvmsg(socket, buf))
+        .await
+}
+
+pub async fn unixgram_recvmsg<B: BufMut>(
+    socket: &mut UnixDatagram, buf: &mut B,
+) -> io::Result<(usize, ConnectionAddress)> {
+    // We manually call `recvmsg` on our domain socket as stdlib/`mio`/`tokio` don't yet expose a way to do out-of-band
+    // reads to get ancillary data such as the socket credentials used to shuttle origin detection information.
+    socket
+        .async_io(tokio::io::Interest::READABLE, || uds_recvmsg(socket, buf))
+        .await
 }
--- lib/saluki-io/src/net/unix/non_linux.rs
+++ lib/saluki-io/src/net/unix/non_linux.rs
@@ -8,3 +8,40 @@ where
 {
     Ok(())
 }
+
+pub(super) fn uds_recvmsg<'sock, S, B: BufMut>(socket: &'sock S, buf: &mut B) -> io::Result<(usize, ConnectionAddress)>
+where
+    SockRef<'sock>: From<&'sock S>,
+{
+    let sock_ref = socket2::SockRef::from(socket);
+
+    // Create the message header struct that will be populated by the call to `recvmsg`, which includes the peer
+    // address, message data, and any ancillary (out-of-band) data.
+    //
+    // SAFETY: We're allocating `sockaddr_storage`, which is always large enough to hold any address family's socket
+    // address structure.
+    //
+    // TODO: We could probably scope this down to `sockaddr_un`, to do a smaller stack allocation... but I need to
+    // research more if that's the right choice in all cases for a Unix domain socket.
+    let sock_storage: libc::sockaddr_storage = unsafe { mem::zeroed() };
+    let sock_storage_len = mem::size_of_val(&sock_storage) as libc::socklen_t;
+    let mut sock_addr = unsafe { SockAddr::new(sock_storage, sock_storage_len) };
+
+    let mut ancillary_data = SocketCredentialsAncillaryData::new();
+
+    let data_buf = unsafe { socket2::MaybeUninitSlice::new(buf.chunk_mut().as_uninit_slice_mut()) };
+    let mut data_bufs = [data_buf];
+
+    let mut msg_hdr = socket2::MsgHdrMut::new()
+        .with_addr(&mut sock_addr)
+        .with_buffers(&mut data_bufs);
+
+    let n = sock_ref.recvmsg(&mut msg_hdr, 0)?;
+
+    // Finally, update our buffer to reflect the bytes we've read.
+    unsafe {
+        buf.advance_mut(n);
+    }
+
+    Ok((n, ConnectionAddress::ProcessLike(None)))
+}
