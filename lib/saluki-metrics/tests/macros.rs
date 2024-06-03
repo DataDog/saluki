5b66ae29bab8bed763709e3c909a2884bd20df75
Toby Lawrence <toby@nuclearfurnace.com>
2024-06-03 13:25:37 +0000

    fmt and licenses

--- lib/saluki-metrics/tests/macros.rs
+++ lib/saluki-metrics/tests/macros.rs
@@ -4,5 +4,5 @@ pub fn macros() {
     t.pass("tests/macros/01_basic_usage.rs");
     t.pass("tests/macros/02_trailing_commas.rs");
     t.pass("tests/macros/03_no_labels.rs");
-	t.compile_fail("tests/macros/04_no_metrics.rs");
+    t.compile_fail("tests/macros/04_no_metrics.rs");
 }
