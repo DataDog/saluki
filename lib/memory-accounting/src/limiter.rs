1e45b4ace04da5ddb44f6e150ee67865db4bf919
Toby Lawrence <toby@nuclearfurnace.com>
2024-06-07 19:18:47 +0000

    rework limiter to progressively apply backpressure

--- lib/memory-accounting/src/limiter.rs
+++ lib/memory-accounting/src/limiter.rs
@@ -1,55 +1,14 @@
 use std::{
     sync::{
-        atomic::{
-            AtomicBool,
-            Ordering::{AcqRel, Acquire},
-        },
+        atomic::{AtomicUsize, Ordering::Relaxed},
         Arc,
     },
     time::Duration,
 };
 
-use tokio::sync::Notify;
 use tracing::debug;
 
 use crate::MemoryGrant;
-
-struct State {
-    gate_closed: AtomicBool,
-    notifier: Notify,
-}
-
-impl State {
-    /// Returns `false` if the gate was already closed.
-    fn close_gate(&self) -> bool {
-        !self.gate_closed.swap(true, AcqRel)
-    }
-
-    /// Returns `false`	if the gate was already open.
-    fn open_gate(&self) -> bool {
-        if self.gate_closed.swap(false, AcqRel) {
-            self.notifier.notify_waiters();
-            true
-        } else {
-            false
-        }
-    }
-
-    async fn wait_for_gate(&self) {
-        if self.gate_closed.load(Acquire) {
-            // The gate is closed, but we want to be sure we're not just catching it right before it opens, since we'd
-            // miss the wake up.
-            //
-            // We grab our notify future, which when used when `notify_waiters` is **guaranteed** to be woken up as soon
-            // as it is constructed, and then we check `gate_closed` again. If it's still `true`, then we know we need
-            // to wait to be notified. Otherwise, we can skip waiting for a notification entirely.
-            let notified = self.notifier.notified();
-            if self.gate_closed.load(Acquire) {
-                notified.await;
-            }
-        }
-    }
-}
 
 /// A process-wide memory limiter.
 ///
@@ -57,40 +16,55 @@ impl State {
 /// be able to react that: clearing caches, temporarily blocking requests, and so on.
 ///
 /// `MemoryLimiter` watches the process's physical memory usage (Resident Set Size/Working Set) and keeps track of when
-/// the usage exceeds the configured limit. This is coupled with an underlying mechanism for allowing interested parties
-/// to wait for the memory usage to drop below the limit by calling `wait_for_capacity`.
+/// the usage exceeds the configured limit. Cooperating tasks call `wait_for_capacity` to participate in accepting
+/// backpressure that can be used to throttle work that is responsible for allocating memory.
 ///
-/// When the limit has not been exceeded, these calls return immediately. However, when the limit has been exceeded,
-/// they will block until the memory usage drops below the limit. This allows components to cooperatively participate in
-/// trying to stay below the configured limit in a straightforward way.
+/// The backpressure is scaled based on how close the memory usage is to the configured limit, with a minimum and
+/// maximum backoff duration that is applied. This means that until we are using a certain percentage of the configured
+/// limit, no backpressure is applied. Once that threshold is crossed, backpressure is applied proportionally to how
+/// close to the limit we are. Callers are never fully blocked even if the limit is reached.
 #[derive(Clone)]
 pub struct MemoryLimiter {
-    state: Arc<State>,
+    rss_limit: usize,
+    actual_rss: Arc<AtomicUsize>,
+    backoff_threshold: f64,
+    backoff_min: Duration,
+    backoff_max: Duration,
 }
 
 impl MemoryLimiter {
     /// Creates a new `MemoryLimiter` based on the given `MemoryGrant`.
     ///
-    /// A background task is spawned that frequently checks the used memory for the entire process and will block
-    /// callers of `wait_for_capacity` until the used memory is below the effective limit of `grant.`
+    /// A background task is spawned that frequently checks the used memory for the entire process, and callers of
+    /// `wait_for_capacity` will observe waiting once the memory usage exceeds the configured limit threshold. The
+    /// waiting time will scale progressively the closer the memory usage is to the configured limit.
+    ///
+    /// Defaults to a 90% threshold (i.e. threshold begins at 90% of the limit), a minimum backoff duration of 1ms, and
+    /// a maximum backoff duration of 25ms. The effective limit of the grant is used as the memory limit.
     pub fn new(grant: MemoryGrant) -> Option<Self> {
-        if memory_stats::memory_stats().is_none() {
-            return None;
-        }
+        // Smoke test to see if we can even collect memory stats on this system.
+        memory_stats::memory_stats()?;
 
-        let state = Arc::new(State {
-            gate_closed: AtomicBool::new(false),
-            notifier: Notify::new(),
-        });
+        let rss_limit = grant.effective_limit_bytes();
+        let backoff_threshold = 0.9;
+        let backoff_min = Duration::from_millis(1);
+        let backoff_max = Duration::from_millis(25);
 
-        let state2 = Arc::clone(&state);
+        let actual_rss = Arc::new(AtomicUsize::new(0));
+        let actual_rss2 = Arc::clone(&actual_rss);
 
         std::thread::Builder::new()
             .name("memory-limiter-checker".to_string())
-            .spawn(move || check_memory_usage(state2, grant))
+            .spawn(move || check_memory_usage(actual_rss2, rss_limit, backoff_threshold, backoff_min, backoff_max))
             .unwrap();
 
-        Some(Self { state })
+        Some(Self {
+            rss_limit,
+            actual_rss,
+            backoff_threshold,
+            backoff_min,
+            backoff_max,
+        })
     }
 
     /// Creates a no-op `MemoryLimiter` that does not perform any limiting.
@@ -98,54 +72,71 @@ impl MemoryLimiter {
     /// All calls to `wait_for_capacity` will return immediately.
     pub fn noop() -> Self {
         Self {
-            state: Arc::new(State {
-                gate_closed: AtomicBool::new(false),
-                notifier: Notify::new(),
-            }),
+            rss_limit: usize::MAX,
+            actual_rss: Arc::new(AtomicUsize::new(0)),
+            backoff_threshold: 1.0,
+            backoff_min: Duration::new(0, 0),
+            backoff_max: Duration::new(0, 0),
         }
     }
 
-    /// Checks the limiter to ensure there is available memory capacity.
+    /// Waits a short period of time based on the available memory.
     ///
-    /// If there is no capacity currently, it waits until capacity becomes available.
+    /// If the used memory is not within the threshold of the configured limit, no waiting will occur. Otherwise, the
+    /// call will wait a variable amount of time depending on how close to the configured limit the process is.
     pub async fn wait_for_capacity(&self) {
-        self.state.wait_for_gate().await;
+        let actual_rss = self.actual_rss.load(Relaxed);
+        if let Some(backoff_dur) = calculate_backoff(
+            self.rss_limit,
+            actual_rss,
+            self.backoff_threshold,
+            self.backoff_min,
+            self.backoff_max,
+        ) {
+            tokio::time::sleep(backoff_dur).await;
+        }
     }
 }
 
-fn check_memory_usage(state: Arc<State>, grant: MemoryGrant) {
-    // We use the _initial_ limit here, because despite the doc comment for it, it should represent the high-level limit
-    // given to the process... which is what we want to limit against. The effective limit is calculated for the purpose
-    // of validating bounds because we know we're likely to exceed it given the lossy approach to defining them by hand.
-    let rss_limit = grant.initial_limit_bytes();
-    debug!(rss_limit, "Memory limiter checker started.");
+fn check_memory_usage(
+    state: Arc<AtomicUsize>, rss_limit: usize, backoff_threshold: f64, backoff_min: Duration, backoff_max: Duration,
+) {
+    debug!("Memory limiter checker started.");
 
     loop {
-        // Check the physical memory (RSS, or Working Set in Windows) and close the gate if we're over the limit.
-        //
-        // When we close the gate, or when the gate is still closed, we'll check more frequently since the implication
-        // is that we're actively blocking components from doing further work, so we want to let them get back to work
-        // as soon as possible... but otherwise, we check less frequently just to avoid constantly flapping between
-        // open/closed if we're close to the limit.
         let mem_stats = memory_stats::memory_stats().expect("memory statistics should be available");
-        let recheck_duration = if mem_stats.physical_mem > rss_limit {
-            debug!(
-                rss_limit,
-                actual_rss = mem_stats.physical_mem,
-                "Memory usage exceeded limit. Gate closed."
-            );
-            if state.close_gate() {}
-            Duration::from_millis(250)
-        } else {
-            debug!(
-                rss_limit,
-                actual_rss = mem_stats.physical_mem,
-                "Memory usage below limit. Gate opened."
-            );
-            if state.open_gate() {}
-            Duration::from_millis(1000)
-        };
+        let actual_rss = mem_stats.physical_mem;
+        state.store(actual_rss, Relaxed);
 
-        std::thread::sleep(recheck_duration);
+        if let Some(backoff_dur) = calculate_backoff(rss_limit, actual_rss, backoff_threshold, backoff_min, backoff_max)
+        {
+            debug!(rss_limit, actual_rss, current_backoff = ?backoff_dur, "Enforcing backoff due to memory pressure.");
+        }
+
+        std::thread::sleep(Duration::from_millis(250));
+    }
+}
+
+fn calculate_backoff(
+    rss_limit: usize, actual_rss: usize, backoff_threshold: f64, backoff_min: Duration, backoff_max: Duration,
+) -> Option<Duration> {
+    if actual_rss as f64 > rss_limit as f64 * backoff_threshold {
+        // When we're over the threshold, we want to scale our backoff range across the remainder of the threshold.
+        //
+        // For example, if our minimum and maximum backoff durations are 100ms and 1000ms, respectively, and our limit
+        // is 100MB with a threshold of 95% (0.95), then we would start backing off by 100ms when we hit 95MB, and we
+        // would back off at a maximum of 1000ms at 100% (or greater) of our limit.
+        //
+        // Between those two points, we would spread the difference of the minimum/maximum backoff duration (1000ms -
+        // 100ms => 900ms) across that 5%. Thus, we would expect that at 97.5% of the limit, we would be backing off by
+        // 550ms. (0.5 * 900ms => 450ms, 100ms + 450ms => 550ms)
+        let rss_backoff_range = rss_limit as f64 - (rss_limit as f64 * backoff_threshold);
+        let backoff_duration_range = backoff_max - backoff_min;
+        let threshold_delta = actual_rss as f64 - (rss_limit as f64 * backoff_threshold);
+        let variable_backoff_duration = backoff_duration_range.as_secs_f64() * (threshold_delta / rss_backoff_range);
+
+        Some(backoff_min + Duration::from_secs_f64(variable_backoff_duration))
+    } else {
+        None
     }
 }
