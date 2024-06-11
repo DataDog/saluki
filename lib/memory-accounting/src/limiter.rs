2708d00aaf590e01699fe03995995efd9ee543f2
Toby Lawrence <toby@nuclearfurnace.com>
2024-06-11 17:47:13 +0000

    memory-accounting: adjust limiter to load backoff duration directly to avoid constantly recalculating + metrics for active backoff dur

--- lib/memory-accounting/src/limiter.rs
+++ lib/memory-accounting/src/limiter.rs
@@ -1,11 +1,12 @@
 use std::{
     sync::{
-        atomic::{AtomicUsize, Ordering::Relaxed},
+        atomic::{AtomicU64, Ordering::Relaxed},
         Arc,
     },
     time::Duration,
 };
 
+use metrics::gauge;
 use tracing::debug;
 
 use crate::MemoryGrant;
@@ -25,11 +26,7 @@ use crate::MemoryGrant;
 /// close to the limit we are. Callers are never fully blocked even if the limit is reached.
 #[derive(Clone)]
 pub struct MemoryLimiter {
-    rss_limit: usize,
-    actual_rss: Arc<AtomicUsize>,
-    backoff_threshold: f64,
-    backoff_min: Duration,
-    backoff_max: Duration,
+    active_backoff_nanos: Arc<AtomicU64>,
 }
 
 impl MemoryLimiter {
@@ -50,21 +47,15 @@ impl MemoryLimiter {
         let backoff_min = Duration::from_millis(1);
         let backoff_max = Duration::from_millis(25);
 
-        let actual_rss = Arc::new(AtomicUsize::new(0));
-        let actual_rss2 = Arc::clone(&actual_rss);
+        let active_backoff_nanos = Arc::new(AtomicU64::new(0));
+        let active_backoff_nanos2 = Arc::clone(&active_backoff_nanos);
 
         std::thread::Builder::new()
             .name("memory-limiter-checker".to_string())
-            .spawn(move || check_memory_usage(actual_rss2, rss_limit, backoff_threshold, backoff_min, backoff_max))
+            .spawn(move || check_memory_usage(active_backoff_nanos2, rss_limit, backoff_threshold, backoff_min, backoff_max))
             .unwrap();
 
-        Some(Self {
-            rss_limit,
-            actual_rss,
-            backoff_threshold,
-            backoff_min,
-            backoff_max,
-        })
+        Some(Self { active_backoff_nanos })
     }
 
     /// Creates a no-op `MemoryLimiter` that does not perform any limiting.
@@ -72,11 +63,7 @@ impl MemoryLimiter {
     /// All calls to `wait_for_capacity` will return immediately.
     pub fn noop() -> Self {
         Self {
-            rss_limit: usize::MAX,
-            actual_rss: Arc::new(AtomicUsize::new(0)),
-            backoff_threshold: 1.0,
-            backoff_min: Duration::new(0, 0),
-            backoff_max: Duration::new(0, 0),
+            active_backoff_nanos: Arc::new(AtomicU64::new(0)),
         }
     }
 
@@ -85,32 +72,36 @@ impl MemoryLimiter {
     /// If the used memory is not within the threshold of the configured limit, no waiting will occur. Otherwise, the
     /// call will wait a variable amount of time depending on how close to the configured limit the process is.
     pub async fn wait_for_capacity(&self) {
-        let actual_rss = self.actual_rss.load(Relaxed);
-        if let Some(backoff_dur) = calculate_backoff(
-            self.rss_limit,
-            actual_rss,
-            self.backoff_threshold,
-            self.backoff_min,
-            self.backoff_max,
-        ) {
-            tokio::time::sleep(backoff_dur).await;
+        let active_backoff_nanos = self.active_backoff_nanos.load(Relaxed);
+        if active_backoff_nanos > 0 {
+            tokio::time::sleep(Duration::from_nanos(active_backoff_nanos)).await;
         }
     }
 }
 
 fn check_memory_usage(
-    state: Arc<AtomicUsize>, rss_limit: usize, backoff_threshold: f64, backoff_min: Duration, backoff_max: Duration,
+    active_backoff_nanos: Arc<AtomicU64>, rss_limit: usize, backoff_threshold: f64, backoff_min: Duration, backoff_max: Duration,
 ) {
     debug!("Memory limiter checker started.");
 
     loop {
         let mem_stats = memory_stats::memory_stats().expect("memory statistics should be available");
         let actual_rss = mem_stats.physical_mem;
-        state.store(actual_rss, Relaxed);
 
-        if let Some(backoff_dur) = calculate_backoff(rss_limit, actual_rss, backoff_threshold, backoff_min, backoff_max)
-        {
-            debug!(rss_limit, actual_rss, current_backoff = ?backoff_dur, "Enforcing backoff due to memory pressure.");
+        let maybe_backoff_duration =
+            calculate_backoff(rss_limit, actual_rss, backoff_threshold, backoff_min, backoff_max);
+        match maybe_backoff_duration {
+            Some(backoff_dur) => {
+                active_backoff_nanos.store(backoff_dur.as_nanos() as u64, Relaxed);
+
+                debug!(rss_limit, actual_rss, current_backoff = ?backoff_dur, "Enforcing backoff due to memory pressure.");
+                gauge!("memory_limiter.current_backoff_secs").set(backoff_dur.as_secs_f64());
+            }
+            None => {
+                active_backoff_nanos.store(0, Relaxed);
+
+                gauge!("memory_limiter.current_backoff_secs").set(0.0);
+            }
         }
 
         std::thread::sleep(Duration::from_millis(250));
