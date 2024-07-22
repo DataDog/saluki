84e7620c502eac2d83d756c2386c6cb6a16602b9
Toby Lawrence <toby@nuclearfurnace.com>
2024-07-22 23:52:11 +0000

    chore: clean up eventd and service check types

--- lib/saluki-event/src/eventd/mod.rs
+++ lib/saluki-event/src/eventd/mod.rs
@@ -1,9 +1,33 @@
-//! Event types
-mod priority;
-pub use self::priority::*;
+//! Events.
 
-mod alert;
-pub use self::alert::*;
+// TODO: Switch usages of `String` to `MetaString` since we should generally be able to intern these strings as they
+// originate in in the DogStatsD codec, where interning is already taking place.
+
+/// Alert type.
+#[derive(Clone, Copy, Debug)]
+pub enum AlertType {
+    /// Indicates an informational event.
+    Info,
+
+    /// Indicates an error event.
+    Error,
+
+    /// Indicates a warning event.
+    Warning,
+
+    /// Indicates a successful event.
+    Success,
+}
+
+/// Event priority.
+#[derive(Clone, Copy, Debug)]
+pub enum Priority {
+    /// The event has normal priority.
+    Normal,
+
+    /// The event has low priority.
+    Low,
+}
 
 /// EventD is an object that can be posted to the DataDog event stream.
 #[derive(Clone, Debug)]
@@ -13,55 +37,57 @@ pub struct EventD {
     timestamp: u64,
     host: String,
     aggregation_key: String,
-    priority: EventPriority,
+    priority: Priority,
     source_type_name: String,
-    alert_type: EventAlertType,
+    alert_type: AlertType,
     tags: Vec<String>,
 }
 
 impl EventD {
-    /// Gets a reference to the title.
+    /// Gets the title of the event.
     pub fn title(&self) -> &str {
         &self.title
     }
 
-    /// Gets a reference to the text.
+    /// Gets the text of the event.
     pub fn text(&self) -> &str {
         &self.text
     }
 
-    /// Gets a reference to the host.
+    /// Gets the host where the event originated from.
     pub fn host(&self) -> &str {
         &self.host
     }
 
-    /// Gets a reference to the aggregation key.
+    /// Gets the aggregation key of the event.
     pub fn aggregation_key(&self) -> &str {
         &self.aggregation_key
     }
 
-    /// Gets a reference to the priority.
-    pub fn priority(&self) -> &EventPriority {
-        &self.priority
+    /// Gets the priority of the event.
+    pub fn priority(&self) -> Priority {
+        self.priority
     }
 
-    /// Gets a reference to the source type name.
+    /// Gets the source type name of the event.
     pub fn source_type_name(&self) -> &str {
         &self.source_type_name
     }
 
-    /// Gets a reference to the alert type
-    pub fn alert_type(&self) -> &EventAlertType {
-        &self.alert_type
+    /// Gets the alert type of the event.
+    pub fn alert_type(&self) -> AlertType {
+        self.alert_type
     }
 
-    /// Gets a reference to the timestamp.
-    pub fn timestamp(&self) -> &u64 {
-        &self.timestamp
+    /// Gets the timestamp of the event.
+    ///
+    /// This is a Unix timestamp, or the number of seconds since the Unix epoch.
+    pub fn timestamp(&self) -> u64 {
+        self.timestamp
     }
 
-    /// Gets a reference to the tags.
-    pub fn tags(&self) -> &Vec<String> {
+    /// Gets the tags associated with the event.
+    pub fn tags(&self) -> &[String] {
         &self.tags
     }
 }
--- lib/saluki-event/src/service_check/mod.rs
+++ lib/saluki-event/src/service_check/mod.rs
@@ -1,12 +1,32 @@
-//! Service Check type
-mod status;
-pub use self::status::*;
+//! Service checks.
 
-/// ServiceCheck
+// TODO: Switch usages of `String` to `MetaString` since we should generally be able to intern these strings as they
+// originate in in the DogStatsD codec, where interning is already taking place.
+
+/// Service status.
+#[derive(Clone, Copy, Debug)]
+pub enum CheckStatus {
+    /// The service is operating normally.
+    Ok,
+
+    /// The service is in a warning state.
+    Warn,
+
+    /// The service is in a critical state.
+    Critical,
+
+    /// The service is in an unknown state.
+    Unknown,
+}
+
+/// A service check.
+///
+/// Service checks represent the status of a service at a particular point in time. Checks are simplistic, with a basic
+/// message, status enum (OK vs warning vs critical, etc), timestamp, and tags.
 #[derive(Clone, Debug)]
 pub struct ServiceCheck {
     name: String,
-    status: ServiceCheckStatus,
+    status: CheckStatus,
     timestamp: u64,
     host: String,
     message: String,
@@ -14,33 +34,35 @@ pub struct ServiceCheck {
 }
 
 impl ServiceCheck {
-    /// Gets a reference to the service check name
-    pub fn name(&self) -> &String {
+    /// Gets the name of the check.
+    pub fn name(&self) -> &str {
         &self.name
     }
 
-    /// Gets a reference to the status
-    pub fn status(&self) -> &ServiceCheckStatus {
-        &self.status
+    /// Gets the status of the check.
+    pub fn status(&self) -> CheckStatus {
+        self.status
     }
 
-    /// Gets a reference to the timestamp
+    /// Gets the timestamp of the check.
+    ///
+    /// This is a Unix timestamp, or the number of seconds since the Unix epoch.
     pub fn timestamp(&self) -> u64 {
         self.timestamp
     }
 
-    /// Gets a reference to the host name
-    pub fn host(&self) -> &String {
+    /// Gets the host where the check originated from.
+    pub fn host(&self) -> &str {
         &self.host
     }
 
-    /// Gets a reference to the message
-    pub fn message(&self) -> &String {
+    /// Gets the message associated with the check.
+    pub fn message(&self) -> &str {
         &self.message
     }
 
-    /// Gets a reference to the tags
-    pub fn tags(&self) -> &Vec<String> {
+    /// Gets the tags associated with the check.
+    pub fn tags(&self) -> &[String] {
         &self.tags
     }
 }
