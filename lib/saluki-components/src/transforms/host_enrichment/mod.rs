8a0c37a39b13ea1be0f45676adeff48487926f8d
Toby Lawrence <tobz@users.noreply.github.com>
2024-07-23 10:06:54 -0400

    chore: clean up eventd and service check types (#130)

--- lib/saluki-components/src/transforms/host_enrichment/mod.rs
+++ lib/saluki-components/src/transforms/host_enrichment/mod.rs
@@ -71,7 +71,7 @@ impl HostEnrichment {
 impl SynchronousTransform for HostEnrichment {
     fn transform_buffer(&self, event_buffer: &mut EventBuffer) {
         for event in event_buffer {
-            if let Some(metric) = event.as_metric_mut() {
+            if let Some(metric) = event.try_as_metric_mut() {
                 self.enrich_metric(metric)
             }
         }
