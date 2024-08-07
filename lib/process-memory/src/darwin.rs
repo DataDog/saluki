a85b74e9d091341c2db3a8cbf411f825a81ddb39
Toby Lawrence <tobz@users.noreply.github.com>
2024-08-07 11:49:53 -0400

    [APR-205] app: eliminate per-call allocations for querying process RSS (#143)

---
+++ lib/process-memory/src/darwin.rs
@@ -0,0 +1,61 @@
+use std::mem::MaybeUninit;
+
+use libc::{
+    mach_msg_type_number_t, mach_task_basic_info_data_t, mach_task_self, task_info, task_info_t, KERN_SUCCESS,
+    MACH_TASK_BASIC_INFO, MACH_TASK_BASIC_INFO_COUNT,
+};
+
+/// A memory usage querier.
+#[derive(Default)]
+pub struct Querier {
+    _priv: (),
+}
+
+impl Querier {
+    /// Gets the resident set size of this process, in bytes.
+    ///
+    /// If the resident set size cannot be determined, `None` is returned. This could be for a number of underlying
+    /// reasons, but should generally be considered an incredibly rare/unlikely event.
+    pub fn resident_set_size(&mut self) -> Option<usize> {
+        // Prepare a holding struct for the task info.
+        //
+        // This represents a set of integers, each relating to a specific task value, and `task_info` expects a pointer to
+        // this struct and the number of integers it is able to write into it, which is already derived for us in
+        // `MACH_TASK_BASIC_INFO_COUNT`.
+        let mut basic_task_info = MaybeUninit::<mach_task_basic_info_data_t>::uninit();
+        let mut basic_task_info_len = MACH_TASK_BASIC_INFO_COUNT;
+
+        // SAFETY: We're passing a valid pointer, and struct length, for the task info output.
+        let result = unsafe {
+            task_info(
+                mach_task_self(),
+                MACH_TASK_BASIC_INFO,
+                basic_task_info.as_mut_ptr() as task_info_t,
+                &mut basic_task_info_len as *mut mach_msg_type_number_t,
+            )
+        };
+        match result {
+            KERN_SUCCESS => {
+                // SAFETY: We know the structure has been populated by `task_info` at this point.
+                let basic_task_info = unsafe { basic_task_info.assume_init() };
+                Some(basic_task_info.resident_size as usize)
+            }
+
+            // Failed to get the task info.
+            //
+            // This could be for a number of reasons, but should generally be considered an incredibly rare/unlikely event.
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Querier;
+
+    #[test]
+    fn basic() {
+        let mut querier = Querier::default();
+        assert!(querier.resident_set_size().is_some());
+    }
+}
---
+++ lib/process-memory/src/lib.rs
@@ -0,0 +1,48 @@
+//! Process memory querying.
+//!
+//! This crate provides a cross-platform way to query the RSS (resident set size) usage of a process.
+//!
+//! ## Linux
+//!
+//! On Linux, [procfs](https://docs.kernel.org/filesystems/proc.html) is used, and one of three files may be read,
+//! depending on their availability:
+//!
+//! - `/proc/self/smaps_rollup`: This file is a pre-aggregated version of `/proc/self/smaps`, and is the most efficient way
+//!   to query RSS. (Available in Linux 4.14+)
+//! - `/proc/self/smaps`: This file contains detailed information about the memory mappings of the process, and can be
+//!   aggregated to determine the resident set size. (Available in Linux 2.6.14+)
+//! - `/proc/self/statm`: This file contains lazily-updated memory statistics about the process, and is the least
+//!   accurate, but is generally good enough for most use-cases. (Available in Linux 2.6+)
+//!
+//! ## macOS
+//!
+//! On macOS, we query the kernel directly for Mach task information.
+//!
+//! Available since Mac OS X 10.0 (Cheetah).
+//!
+//! ## Windows
+//!
+//! On Windows, we query the kernel directly for process memory counters.
+//!
+//! Available since Windows XP/Server 2003.
+
+#[cfg(target_os = "linux")]
+mod linux;
+
+#[cfg(target_os = "linux")]
+pub use linux::Querier;
+
+#[cfg(target_os = "macos")]
+mod darwin;
+
+#[cfg(target_os = "macos")]
+pub use darwin::Querier;
+
+#[cfg(target_os = "windows")]
+mod windows;
+
+#[cfg(target_os = "windows")]
+pub use windows::Querier;
+
+#[cfg(all(not(target_os = "linux"), not(target_os = "macos"), not(target_os = "windows")))]
+compile_error!("This crate only supports Linux, macOS, and Windows at the moment.");
