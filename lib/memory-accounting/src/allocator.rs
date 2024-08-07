e969684cb65209125392229dad10d4bbdd9aaa01
Toby Lawrence <toby@nuclearfurnace.com>
2024-08-07 00:25:07 +0000

    wip cleanup

--- lib/memory-accounting/src/allocator.rs
+++ lib/memory-accounting/src/allocator.rs
@@ -1,8 +1,6 @@
-//! A global allocator that tracks allocations and deallocations and attributes them to specific components.
-
-// TODO: The current design does not allow for deregistering components, which is currently fine and likely will be for
-// a while, but would be a limitation in a world where we dynamically launched data pipelines and wanted to clean up
-// removed components, and so on.
+// TODO: The current design does not allow for deregistering groups, which is currently fine and
+// likely will be for a while, but would be a limitation in a world where we dynamically launched
+// data pipelines and wanted to clean up removed components, and so on.
 
 use std::{
     alloc::{GlobalAlloc, Layout},
@@ -23,14 +21,47 @@ use pin_project::pin_project;
 
 const STATS_LAYOUT: Layout = Layout::new::<*const AllocationStats>();
 
-static REGISTRY: OnceLock<ComponentRegistry> = OnceLock::new();
-static ROOT_COMPONENT: AllocationStats = AllocationStats::new();
+static REGISTRY: OnceLock<GroupRegistry> = OnceLock::new();
+static ROOT_GROUP: AllocationStats = AllocationStats::new();
 
 thread_local! {
-    static CURRENT_COMPONENT: RefCell<NonNull<AllocationStats>> = RefCell::new(NonNull::from(&ROOT_COMPONENT));
+    static CURRENT_GROUP: RefCell<NonNull<AllocationStats>> = RefCell::new(NonNull::from(&ROOT_GROUP));
 }
 
-/// A global allocator that tracks allocations and deallocations and attributes them to specific components.
+/// A global allocator that tracks allocations on a per-group basis.
+///
+/// This allocator provides the ability to track the allocations and deallocations, both in bytes
+/// and objects, of different allocation "groups." This allows for the tracking of memory usage
+/// (over time and live) of different components in a system, where the grouping is user-defined.
+///
+/// ## Allocation groups
+///
+/// Allocation statistics are tracked by "group." Groups are registered by callers and
+/// `AllocationGroupToken` is provided so that the caller may enter and exit the group in order to
+/// track allocations.
+///
+/// All allocations are associated with an allocation group. If a user-defined allocation group
+/// isn't being used at the time, allocations are associated to the "root" allocation group, which
+/// is meant to capture all allocations that aren't explicitly attributed to a user-defined group,
+/// along with any allocations that are made in order to store information about allocation groups
+/// themselves.
+///
+/// ## Token guard
+///
+/// When an allocation is registered, `AllocationGroupToken` is returned. This token can be used to
+/// "enter" the group, and attribute all allocations on the current thread to that group for as long
+/// as the token is entered.
+///
+/// When the guard is dropped, the previously entered group is restored. This allows for arbitrary
+/// nesting depth of allocation groups.
+///
+/// ## Changes to memory layout
+///
+/// In order to associate an allocation with the current allocation group, a small trailer is added
+/// to the requested allocation, in the form of a pointer to the statistics for the allocation
+/// group. This allows updating the statistics directly when an allocation is deallocated, without
+/// having to externally keep track of what group a given allocation belongs to. These statistics
+/// are updated directly when the allocation is initially made, and when it is deallocated.
 pub struct TrackingAllocator<A> {
     allocator: A,
 }
@@ -48,43 +79,43 @@ where
 {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         // Adjust the requested layout to fit our trailer and then try and allocate it.
-        let (layout, trailer_start) = get_layout_with_component_trailer(layout);
+        let (layout, trailer_start) = get_layout_with_group_trailer(layout);
         let layout_size = layout.size();
         let ptr = self.allocator.alloc(layout);
         if ptr.is_null() {
             return ptr;
         }
 
-        // Store the pointer to the current component in the trailer, and also update the statistics.
+        // Store the pointer to the current allocation group in the trailer, and also update the statistics.
         let trailer_ptr = ptr.add(trailer_start) as *mut *mut AllocationStats;
-        CURRENT_COMPONENT.with(|current_component| {
-            let component_ptr = current_component.borrow();
-            component_ptr.as_ref().track_allocation(layout_size);
+        CURRENT_GROUP.with(|current_group| {
+            let group_ptr = current_group.borrow();
+            group_ptr.as_ref().track_allocation(layout_size);
 
-            trailer_ptr.write(component_ptr.as_ptr());
+            trailer_ptr.write(group_ptr.as_ptr());
         });
 
         ptr
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        // Read the pointer to the owning component from the trailer and update the statistics.
-        let (layout, trailer_start) = get_layout_with_component_trailer(layout);
+        // Read the pointer to the owning allocation group from the trailer and update the statistics.
+        let (layout, trailer_start) = get_layout_with_group_trailer(layout);
         let trailer_ptr = ptr.add(trailer_start) as *mut *mut AllocationStats;
-        let component = (*trailer_ptr).as_ref().unwrap();
-        component.track_deallocation(layout.size());
+        let group = (*trailer_ptr).as_ref().unwrap();
+        group.track_deallocation(layout.size());
 
         // Deallocate the memory.
         self.allocator.dealloc(ptr, layout);
     }
 }
 
-fn get_layout_with_component_trailer(layout: Layout) -> (Layout, usize) {
+fn get_layout_with_group_trailer(layout: Layout) -> (Layout, usize) {
     let (new_layout, trailer_start) = layout.extend(STATS_LAYOUT).unwrap();
     (new_layout.pad_to_align(), trailer_start)
 }
 
-/// Allocation statistics for a component.
+/// Statistics for an allocation group.
 pub struct AllocationStats {
     allocated_bytes: AtomicUsize,
     allocated_objects: AtomicUsize,
@@ -102,12 +133,12 @@ impl AllocationStats {
         }
     }
 
-    /// Gets a reference to the root component.
+    /// Gets a reference to the statistics of the root allocation group.
     pub fn root() -> &'static Self {
-        &ROOT_COMPONENT
+        &ROOT_GROUP
     }
 
-    /// Returns `true` if the given component has allocated any memory at all.
+    /// Returns `true` if the given group has allocated any memory at all.
     pub fn has_allocated(&self) -> bool {
         self.allocated_bytes.load(Relaxed) > 0
     }
@@ -126,8 +157,9 @@ impl AllocationStats {
 
     /// Consumes the current value of each statistics, resetting them to zero.
     ///
-    /// Deltas represent the total change in each value since the _last_ delta, and the caller must keep track of these
-    /// changes themselves if they wish to track the total values over the life of the process.
+    /// Deltas represent the total change in each value since the _last_ delta, and the caller must
+    /// keep track of these changes themselves if they wish to track the total values over the life
+    /// of the process.
     pub fn consume(&self) -> AllocationStatsDelta {
         AllocationStatsDelta {
             allocated_bytes: self.allocated_bytes.swap(0, Relaxed),
@@ -138,7 +170,7 @@ impl AllocationStats {
     }
 }
 
-/// Delta allocation statistics for a component.
+/// Delta allocation statistics for a group.
 pub struct AllocationStatsDelta {
     /// Number of allocated bytes since the last delta.
     pub allocated_bytes: usize,
@@ -176,41 +208,42 @@ impl AllocationStatsDelta {
     }
 }
 
-/// A token that allows tracking allocations and deallocations for a specific component.
+/// A token associated with a specific allocation group.
+///
+/// Used to attribute allocations and deallocations to a specific group with a scope guard, or
+/// through helpers provided by the [`TrackExt`] trait.
 #[derive(Clone, Copy)]
-pub struct TrackingToken {
-    component_ptr: NonNull<AllocationStats>,
+pub struct AllocationGroupToken {
+    group_ptr: NonNull<AllocationStats>,
 }
 
-impl TrackingToken {
-    fn new(component_ptr: NonNull<AllocationStats>) -> Self {
-        Self { component_ptr }
+impl AllocationGroupToken {
+    fn new(group_ptr: NonNull<AllocationStats>) -> Self {
+        Self { group_ptr }
     }
 
     fn current() -> Self {
-        CURRENT_COMPONENT.with(|current_component| {
-            let component_ptr = current_component.borrow();
-            Self::new(*component_ptr)
+        CURRENT_GROUP.with(|current_group| {
+            let group_ptr = current_group.borrow();
+            Self::new(*group_ptr)
         })
     }
 
-    /// Gets the token for the root component.
+    /// Returns the token for the root allocation group.
     pub fn root() -> Self {
-        Self::new(NonNull::from(&ROOT_COMPONENT))
+        Self::new(NonNull::from(&ROOT_GROUP))
     }
 
-    /// Enters the tracking context for this token, attributing allocations to the component it represents.
-    ///
-    /// This method returns a guard that will reset the currently-tracked component to its previous value when dropped.
+    /// Enters this allocation group, returning a guard that will exit the allocation group when dropped.
     pub fn enter(&self) -> TrackingGuard<'_> {
-        // Swap the current component to the one we're tracking.
-        CURRENT_COMPONENT.with(|current_component| {
-            let mut component_ptr = current_component.borrow_mut();
-            let previous_component_ptr = *component_ptr;
-            *component_ptr = self.component_ptr;
+        // Swap the current group to the one we're tracking.
+        CURRENT_GROUP.with(|current_group| {
+            let mut group_ptr = current_group.borrow_mut();
+            let previous_group_ptr = *group_ptr;
+            *group_ptr = self.group_ptr;
 
             TrackingGuard {
-                previous_component_ptr,
+                previous_group_ptr,
                 _token: PhantomData,
             }
         })
@@ -218,28 +251,33 @@ impl TrackingToken {
 }
 
 // SAFETY: There's nothing inherently thread-specific about the token.
-unsafe impl Send for TrackingToken {}
+unsafe impl Send for AllocationGroupToken {}
 
-/// A guard that resets the currently-tracked component to its previous value when dropped.
+/// A guard representing an allocation group which has been entered.
+///
+/// When the guard is dropped, the allocation group will be exited and the previously entered
+/// allocation group will be restored.
+///
+/// This is returned by the [`AllocationGroupToken::enter`] method.
 pub struct TrackingGuard<'a> {
-    previous_component_ptr: NonNull<AllocationStats>,
-    _token: PhantomData<&'a TrackingToken>,
+    previous_group_ptr: NonNull<AllocationStats>,
+    _token: PhantomData<&'a AllocationGroupToken>,
 }
 
 impl<'a> Drop for TrackingGuard<'a> {
     fn drop(&mut self) {
-        // Reset the current component to the one that existed before we entered.
-        CURRENT_COMPONENT.with(|current_component| {
-            let mut component_ptr = current_component.borrow_mut();
-            *component_ptr = self.previous_component_ptr;
+        // Reset the current group to the one that existed before we entered.
+        CURRENT_GROUP.with(|current_group| {
+            let mut group_ptr = current_group.borrow_mut();
+            *group_ptr = self.previous_group_ptr;
         });
     }
 }
 
-/// An object wrapper that tracks allocations and attributes them to a specific component.
+/// An object wrapper that tracks allocations and attributes them to a specific group.
 ///
 /// Provides methods and implementations to help ensure that operations against/using the wrapped object have all
-/// allocations properly tracked and attributed to a given component.
+/// allocations properly tracked and attributed to a given group.
 ///
 /// Implements [`Future`] when the wrapped object itself implements [`Future`].
 //
@@ -251,27 +289,15 @@ impl<'a> Drop for TrackingGuard<'a> {
 #[pin_project]
 #[must_use = "futures do nothing unless you `.await` or poll them"]
 pub struct Tracked<Inner> {
-    token: TrackingToken,
+    token: AllocationGroupToken,
 
     #[pin]
     inner: Inner,
 }
 
 impl<Inner> Tracked<Inner> {
-    /// Enters the tracking context for this token, attributing allocations to the component it represents.
-    ///
-    /// This method returns a guard that will reset the currently-tracked component to its previous value when dropped.
-    pub fn enter(&self) -> TrackingGuard<'_> {
-        self.token.enter()
-    }
-
-    /// Gets a reference to the inner object.
-    pub fn inner_ref(&self) -> &Inner {
-        &self.inner
-    }
-
     /// Consumes this object and returns the inner object and tracking token.
-    pub fn into_parts(self) -> (TrackingToken, Inner) {
+    pub fn into_parts(self) -> (AllocationGroupToken, Inner) {
         (self.token, self.inner)
     }
 }
@@ -289,17 +315,62 @@ where
     }
 }
 
-/// Attaches tracking tokens to a [`Future`].
+/// Attaches allocation groups to a [`Future`].
 pub trait Track: Sized {
-    /// Tracks allocations and deallocations for this object using the given token.
-    fn track_allocations(self, token: TrackingToken) -> Tracked<Self> {
+    /// Instruments this type by attaching the given allocation group token, returning a `Tracked` wrapper.
+    ///
+    /// The allocation group will be entered every time the wrapped future is polled.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use memory_accounting::allocator::AllocationGroupToken;
+    /// use memory_accounting::allocator::{AllocationGroupToken, Track};
+    ///
+    /// # async fn doc() {
+    /// let future = async {
+    ///     // All allocations in this future will be attached to the allocation group
+    ///     // represented by `token`...
+    /// };
+    ///
+    /// future
+    ///     .track_allocations(AllocationGroupToken::root())
+    ///     .await
+    /// # }
+    fn track_allocations(self, token: AllocationGroupToken) -> Tracked<Self> {
         Tracked { token, inner: self }
     }
 
-    /// Tracks allocations and deallocations for this object using the current component.
-    fn in_current_component(self) -> Tracked<Self> {
+    /// Instruments this type by attaching the current allocation group, returning a `Tracked` wrapper.
+    ///
+    /// The allocation group will be entered every time the wrapped future is polled.
+    ///
+    /// This can be used to propagate the current allocation group when spawning a new future.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use memory_accounting::allocator::{AllocationGroupToken, Track};
+    ///
+    /// # mod tokio {
+    /// #     pub(super) fn spawn(_: impl std::future::Future) {}
+    /// # }
+    /// # async fn doc() {
+    /// let token = AllocationGroupToken::root();
+    /// let _enter = token.enter();
+    ///
+    /// // ...
+    ///
+    /// let future = async {
+    ///     // All allocations in this future will be attached to the allocation group
+    ///     // represented by `token`...
+    /// };
+    /// tokio::spawn(future.in_current_allocation_group());
+    /// # }
+    /// ```
+    fn in_current_allocation_group(self) -> Tracked<Self> {
         Tracked {
-            token: TrackingToken::current(),
+            token: AllocationGroupToken::current(),
             inner: self,
         }
     }
@@ -307,61 +378,65 @@ pub trait Track: Sized {
 
 impl<T: Sized> Track for T {}
 
-/// A registry of components that can have allocations and deallocations attributed to them.
-pub struct ComponentRegistry {
-    components: Mutex<HashMap<String, Box<AllocationStats>>>,
+/// A registry of allocation groups and the statistics for each of them.
+pub struct GroupRegistry {
+    allocation_groups: Mutex<HashMap<String, Box<AllocationStats>>>,
 }
 
-impl ComponentRegistry {
+impl GroupRegistry {
     fn new() -> Self {
-        with_root_component(|| Self {
-            components: Mutex::new(HashMap::new()),
+        in_root_allocation_group(|| Self {
+            allocation_groups: Mutex::new(HashMap::new()),
         })
     }
 
-    /// Gets a reference to the global component registry.
+    /// Gets a reference to the global allocation group registry.
     pub fn global() -> &'static Self {
-        REGISTRY.get_or_init(ComponentRegistry::new)
+        REGISTRY.get_or_init(Self::new)
     }
 
-    /// Registers a new component with the given name.
+    /// Registers a new allocation group with the given name.
     ///
-    /// Returns a `TrackingToken` that can be used to attribute allocations and deallocations to this component.
-    pub fn register_component<S>(&self, name: S) -> TrackingToken
+    /// Returns a `AllocationGroupToken` that can be used to attribute allocations and deallocations to the
+    /// newly-created allocation group.
+    pub fn register_allocation_group<S>(&self, name: S) -> AllocationGroupToken
     where
         S: Into<String>,
     {
-        with_root_component(|| {
-            let component_stats = Box::new(AllocationStats::new());
-            let token = TrackingToken::new(NonNull::from(&*component_stats));
+        in_root_allocation_group(|| {
+            let allocation_group_stats = Box::new(AllocationStats::new());
+            let token = AllocationGroupToken::new(NonNull::from(&*allocation_group_stats));
 
-            self.components.lock().unwrap().insert(name.into(), component_stats);
+            self.allocation_groups
+                .lock()
+                .unwrap()
+                .insert(name.into(), allocation_group_stats);
 
             token
         })
     }
 
-    /// Visits all components in the registry and calls the given closure with their names and statistics.
-    pub fn visit_components<F>(&self, mut f: F)
+    /// Visits all allocation groups in the registry and calls the given closure with their names and statistics.
+    pub fn visit_allocation_groups<F>(&self, mut f: F)
     where
         F: FnMut(&str, &AllocationStats),
     {
-        with_root_component(|| {
-            f("root", &ROOT_COMPONENT);
+        in_root_allocation_group(|| {
+            f("root", &ROOT_GROUP);
 
-            let components = self.components.lock().unwrap();
-            for (name, stats) in components.iter() {
+            let allocation_groups = self.allocation_groups.lock().unwrap();
+            for (name, stats) in allocation_groups.iter() {
                 f(name, stats);
             }
         });
     }
 }
 
-fn with_root_component<F, R>(f: F) -> R
+fn in_root_allocation_group<F, R>(f: F) -> R
 where
     F: FnOnce() -> R,
 {
-    let token = TrackingToken::root();
+    let token = AllocationGroupToken::root();
     let _enter = token.enter();
     f()
 }
