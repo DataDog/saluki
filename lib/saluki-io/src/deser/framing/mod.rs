c156c398ce0524406d2db4690475fb0011ecb85c
Luke Steensen <luke.steensen@gmail.com>
2024-08-15 16:15:56 -0500

    Start to flatten out framing and codecs
    The goal here is just to start reducing some complexity so that subsequent changes are easier to make and simpler to review. Where previously we had a `Deserializer` wrapping a `Framer` wrapping a `Codec`, we now just have a framer and a codec that are used side-by-side. By avoiding the nesting, we're making it easier to introduce a semi-parsed result struct from the codec without needing to thread reference lifetimes through multiple layers of machinery. Actually implementing that change to the codec's parsing function will be the next step, and from there we will be able to rework the rest of the logic (enrichment, differentiating between message types, forwarding, etc) to be a more straightforward function from that semi-parsed representation to a full event, with fewer intermediate steps and less mutation, as well as our ultimate goal, to easily pass in a string/context interner specific to that socket handler.
    
    Signed-off-by: Luke Steensen <luke.steensen@gmail.com>

--- lib/saluki-io/src/deser/framing/mod.rs
+++ lib/saluki-io/src/deser/framing/mod.rs
@@ -1,6 +1,6 @@
 use snafu::Snafu;
 
-use super::Decoder;
+use crate::buf::ReadIoBuffer;
 
 mod length_delimited;
 pub use self::length_delimited::LengthDelimitedFramer;
@@ -10,10 +10,7 @@ pub use self::newline::NewlineFramer;
 
 #[derive(Debug, Snafu)]
 #[snafu(context(suffix(false)))]
-pub enum FramingError<D: Decoder> {
-    #[snafu(display("decoder error: {}", source))]
-    FailedToDecode { source: D::Error },
-
+pub enum FramingError {
     #[snafu(display(
         "received invalid frame (hit EOF and couldn't not parse frame, {} bytes remaining)",
         buffer_len
@@ -21,8 +18,11 @@ pub enum FramingError<D: Decoder> {
     InvalidFrame { buffer_len: usize },
 }
 
-pub trait Framer<D: Decoder> {
-    type Output: Decoder;
-
-    fn with_decoder(self, decoder: D) -> Self::Output;
+pub trait Framer {
+    /// Attempt to extract the next frame from the buffer.
+    ///
+    /// On success, returns a byte slice of the frame data and the number of bytes to advance the buffer.
+    fn next_frame<'a, 'b, B: ReadIoBuffer>(
+        &'a mut self, buf: &'b B, is_eof: bool,
+    ) -> Result<Option<(&'b [u8], usize)>, FramingError>;
 }
