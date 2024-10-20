d8ca52473e9ba6987d02796c632bffd69a0d0667
Toby Lawrence <toby@nuclearfurnace.com>
2024-10-20 10:56:48 +0000

    bunch of cleanup around analysis

--- bin/correctness/ground-truth/src/analysis.rs
+++ bin/correctness/ground-truth/src/analysis.rs
@@ -1,8 +1,8 @@
 use std::collections::HashSet;
 
-use saluki_error::GenericError;
+use saluki_error::{generic_error, GenericError};
 use stele::Metric;
-use tracing::info;
+use tracing::{error, info};
 
 /// Raw test run results.
 ///
@@ -38,83 +38,177 @@ impl RawTestResults {
     ///
     /// If analysis fails, an error will be returned with specific details.
     pub fn run_analysis(self) -> Result<(), GenericError> {
+        // Consume and normalize the metrics, which ensures they're in a consistent state before any analysis.
         let mut dsd_metrics = self.dsd_metrics;
-        let mut adp_metrics = self.adp_metrics;
+        dsd_metrics.iter_mut().for_each(Metric::normalize);
 
-        let dsd_unfiltered_metrics_len = dsd_metrics.len();
-        let adp_unfiltered_metrics_len = adp_metrics.len();
+        let mut adp_metrics = self.adp_metrics;
+        adp_metrics.iter_mut().for_each(Metric::normalize);
+
+        info!("Received {} total metrics from DogStatsD, and {} total metrics from Agent Data Plane.", dsd_metrics.len(), adp_metrics.len());
 
         // Filter out internal telemetry metrics.
-        dsd_metrics.retain(|metric| !is_internal_telemetry(metric));
-        adp_metrics.retain(|metric| !is_internal_telemetry(metric));
+        filter_internal_telemetry_metrics(&mut dsd_metrics, &mut adp_metrics);
 
-        let dsd_filtered_metrics_len = dsd_unfiltered_metrics_len - dsd_metrics.len();
-        let adp_filtered_metrics_len = adp_unfiltered_metrics_len - adp_metrics.len();
+        // Filter out histogram metrics, since we don't currently emit them the same way in ADP as we do in DSD, so
+        // there's no reason to compare them yet.
+        filter_histogram_metrics(&mut dsd_metrics, &mut adp_metrics);
 
-        info!("Received {} metrics ({} filtered out) from DogStatsD, and {} metrics ({} filtered out) from Agent Data Plane.", dsd_metrics.len(), dsd_filtered_metrics_len, adp_metrics.len(), adp_filtered_metrics_len);
-
-        // Now we'll capture the unique metric series we received from both DSD and ADP, to make sure we're looking
-        // at the exact same metrics before we start comparing their datapoints.
+        // With known outliers filtered out, we can actually compare the metrics between DSD and ADP.
         //
-        // We'll specifically sort the tags for each metric, since having them be in different orders is cancelled out
-        // at the intake layer, and we want to emulate that.
-        let mut dsd_unique_metrics = dsd_metrics
-            .iter()
-            .map(|metric| {
-                let name = metric.name();
-                let mut tags = metric.tags().to_vec();
-                tags.sort();
+        // Our first test will simply be to analyze the number of unique metrics (contexts) overall, since we should be
+        // emitting the same metrics from both DSD and ADP.
+        let dsd_unique_metrics = dsd_metrics.iter().map(|m| m.context()).collect::<HashSet<_>>();
+        let adp_unique_metrics = adp_metrics.iter().map(|m| m.context()).collect::<HashSet<_>>();
 
-                format!("{}{{{}}}", name, tags.join(","))
-            })
-            .collect::<Vec<_>>();
+        if dsd_unique_metrics != adp_unique_metrics {
+            error!(
+                "Mismatch in unique metrics between DogStatsD and Agent Data Plane: {} vs. {}",
+                dsd_unique_metrics.len(),
+                adp_unique_metrics.len()
+            );
 
-        let mut adp_unique_metrics = adp_metrics
-            .iter()
-            .map(|metric| {
-                let name = metric.name();
-                let mut tags = metric.tags().to_vec();
-                tags.sort();
+            let missing_from_adp = dsd_unique_metrics.difference(&adp_unique_metrics).collect::<Vec<_>>();
+            let missing_from_dsd = adp_unique_metrics.difference(&dsd_unique_metrics).collect::<Vec<_>>();
 
-                format!("{}{{{}}}", name, tags.join(","))
-            })
-            .collect::<Vec<_>>();
+            error!("Metrics in DogStatsD but not in Agent Data Plane:");
+            for metric in missing_from_adp {
+                error!("  - {}", metric);
+            }
 
-        dsd_unique_metrics.sort();
-        dsd_unique_metrics.dedup();
+            error!("Metrics in Agent Data Plane but not in DogStatsD:");
+            for metric in missing_from_dsd {
+                error!("  - {}", metric);
+            }
 
-        adp_unique_metrics.sort();
-        adp_unique_metrics.dedup();
-
-        info!(
-            "Received {} unique metrics from DogStatsD and {} unique metrics from Agent Data Plane.",
-            dsd_unique_metrics.len(),
-            adp_unique_metrics.len()
-        );
-
-        let dsd_metrics_set = dsd_unique_metrics.into_iter().collect::<HashSet<_>>();
-        let adp_metrics_set = adp_unique_metrics.into_iter().collect::<HashSet<_>>();
-
-        let metrics_in_dsd_not_in_adp = dsd_metrics_set.difference(&adp_metrics_set).collect::<Vec<_>>();
-        let metrics_in_adp_not_in_dsd = adp_metrics_set.difference(&dsd_metrics_set).collect::<Vec<_>>();
-
-        info!("Metrics in DogStatsD but not in Agent Data Plane:");
-        for metric in metrics_in_dsd_not_in_adp {
-            info!("  - {}", metric);
+            return Err(generic_error!(
+                "Mismatch in metric payloads between DogStatsD and Agent Data Plane."
+            ));
         }
 
-        info!("Metrics in Agent Data Plane but not in DogStatsD:");
-        for metric in metrics_in_adp_not_in_dsd {
-            info!("  - {}", metric);
-        }
+        info!("DogStatsD and Agent Data Plane both emitted the same set of unique metric contexts. Continuing...");
 
-        // TODO: actually merge our metrics together, based on their context (name and tags), and start evaluating the
-        // value of the metrics themselves.
+        // Merge and normalize the metrics, which sorts and deduplicates them. This involves merging multiple instances
+        // of `Metric` with the same context, and within a `Metric`, sorting and merging datapoints with the same
+        // timestamp, using metric type-specific merging logic.
+        //
+        // We'll compare the metric values after this step.
+        let mut dsd_anchored_metrics = dsd_metrics.clone();
+        let mut adp_anchored_metrics = adp_metrics.clone();
+
+        merge_and_normalize_metrics(&mut dsd_anchored_metrics);
+        merge_and_normalize_metrics(&mut adp_anchored_metrics);
+
+        info!("Merged and normalized metrics from DogStatsD ({}) and Agent Data Plane ({}). Comparing values...", dsd_anchored_metrics.len(), adp_anchored_metrics.len());
+
+        compare_metric_values(&dsd_anchored_metrics, &adp_anchored_metrics);
 
         Ok(())
     }
 }
 
+fn compare_metric_values(dsd_metrics: &[Metric], adp_metrics: &[Metric]) {
+    let mut mismatched_count = 0;
+
+    // We can safely assume that the metrics are sorted and deduplicated at this point, so we can simply iterate over
+    // them in lockstep.
+    for (dsd_metric, adp_metric) in dsd_metrics.iter().zip(adp_metrics.iter()) {
+        if dsd_metric != adp_metric {
+            mismatched_count += 1;
+        }
+    }
+
+    if mismatched_count == 0 {
+        info!("All metrics from DogStatsD and Agent Data Plane matched successfully.");
+    } else {
+        error!("{} metrics from DogStatsD and Agent Data Plane did not match.", mismatched_count);
+    }
+}
+
+/// Filters out metrics that match a given predicate.
+///
+/// If `filter` returns `true` for a given metric, it will be removed from the provided `metrics` vector.
+///
+/// Returns all of the metrics that were filtered out.
+fn filter_metrics<F>(metrics: &mut Vec<Metric>, filter: F) -> Vec<Metric>
+where
+    F: Fn(&Metric) -> bool,
+{
+    let mut filtered = metrics.clone();
+    filtered.retain(|metric| !filter(metric));
+    metrics.retain(|metric| filter(metric));
+
+    std::mem::replace(metrics, filtered)
+}
+
+fn filter_internal_telemetry_metrics(dsd_metrics: &mut Vec<Metric>, adp_metrics: &mut Vec<Metric>) {
+    let dsd_filtered_metrics = filter_metrics(dsd_metrics, is_internal_telemetry);
+    let adp_filtered_metrics = filter_metrics(adp_metrics, is_internal_telemetry);
+
+    info!("Filtered {} internal telemetry metric(s) from DogStatsD, and {} internal telemetry metric(s) from Agent Data Plane.", dsd_filtered_metrics.len(), adp_filtered_metrics.len());
+}
+
+fn filter_histogram_metrics(dsd_metrics: &mut Vec<Metric>, adp_metrics: &mut Vec<Metric>) {
+    // First, we do the raw filtering of histogram metrics.
+    let dsd_filtered_metrics = filter_metrics(dsd_metrics, is_histogram_metric);
+    let adp_filtered_metrics = filter_metrics(adp_metrics, is_histogram_metric);
+
+    if !adp_filtered_metrics.is_empty() {
+        panic!("Found histogram metrics in Agent Data Plane. This is unexpected, as we don't currently emit histogram metrics in ADP.");
+    }
+
+    // Next, we examine the metrics we filtered from the DSD side to find the unique metric context once the
+    // histogram-specific suffix is removed. Since the same metrics are going into DSD and ADP, we want to find the
+    // corresponding metric on the ADP side and remove it.
+    //
+    // Essentially, we may find `foo_metric.avg`, `foo_metric.min`, and so on, when filtering from DSD. We would then
+    // expect to see `foo_metric` in the ADP metrics... and that's what we want to remove.
+    let histogram_metric_base_names = dsd_filtered_metrics
+        .iter()
+        .filter_map(|metric| {
+            metric.context().name().rsplit_once('.').map(|(base_name, _)| base_name)
+        })
+        .collect::<HashSet<_>>();
+
+    let adp_filtered_metrics = filter_metrics(adp_metrics, |metric| {
+        histogram_metric_base_names.contains(metric.context().name())
+    });
+
+    info!("Filtered {} histogram metric(s) from DogStatsD, and {} histogram metric(s) from Agent Data Plane.", dsd_filtered_metrics.len(), adp_filtered_metrics.len());
+}
+
+fn merge_and_normalize_metrics(metrics: &mut Vec<Metric>) {
+    // We sort the metrics by context (name and tags) so that we can then deduplicate them.
+    //
+    // During this, the act of merging the values also sorts and deduplicates the values, such that the values will be
+    // sorted by timestamp, with all identical datapoints (identical by timestamp) merged.
+    metrics.sort_by(|a, b| a.context().cmp(b.context()));
+    metrics.dedup_by(|a, b| {
+        // We merge `a` into `b` if they're equal, because `a` is the one that gets removed if they match.
+        if a.context() == b.context() {
+            b.merge_values(a);
+            true
+        } else {
+            false
+        }
+    });
+
+    // We also "anchor" the metric values, which involves adjusting the timestamps so that the first timestamp is 0,
+    // thereby "anchoring" the metric to a fixed point in time. We do this to better compare flushed datapoints between
+    // DSD and ADP.
+    metrics.iter_mut().for_each(Metric::anchor_values); 
+}
+
 fn is_internal_telemetry(metric: &Metric) -> bool {
-    metric.name().starts_with("datadog.") || metric.name().starts_with("n_o_i_n_d_e_x")
+    metric.context().name().starts_with("datadog.") || metric.context().name().starts_with("n_o_i_n_d_e_x")
+}
+
+fn is_histogram_metric(metric: &Metric) -> bool {
+    metric.context().name().ends_with(".min")
+        || metric.context().name().ends_with(".max")
+        || metric.context().name().ends_with(".avg")
+        || metric.context().name().ends_with(".median")
+        || metric.context().name().ends_with(".count")
+        || metric.context().name().ends_with(".sum")
+        || metric.context().name().ends_with(".95percentile")
 }
--- bin/correctness/stele/src/lib.rs
+++ bin/correctness/stele/src/lib.rs
@@ -1,35 +1,123 @@
+use std::fmt;
+
 use datadog_protos::metrics::{MetricPayload, MetricType, SketchPayload};
 use ddsketch_agent::DDSketch;
 use saluki_error::{generic_error, GenericError};
 use serde::{Deserialize, Serialize};
 
-/// A simplified metric representation.
-#[derive(Clone, Deserialize, Serialize)]
-pub struct Metric {
+// A metric's unique identifier.
+#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct MetricContext {
     name: String,
     tags: Vec<String>,
-    values: Vec<(u64, MetricValue)>,
 }
 
-impl Metric {
-    /// Returns the name of the metric.
+impl MetricContext {
+    fn normalize(&mut self) {
+        self.tags.sort();
+        self.tags.dedup();
+    }
+
+    /// Returns the name of the context.
     pub fn name(&self) -> &str {
         &self.name
     }
 
-    /// Returns the tags associated with the metric.
+    /// Returns the tags of the context.
     pub fn tags(&self) -> &[String] {
         &self.tags
+    }
+}
+
+impl fmt::Display for MetricContext {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name)?;
+
+        if !self.tags.is_empty() {
+            write!(f, " {{{}}}", self.tags.join(", "))?;
+        }
+
+        Ok(())
+    }
+}
+
+/// A simplified metric representation.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Metric {
+    context: MetricContext,
+    values: Vec<(u64, MetricValue)>,
+}
+
+impl Metric {
+    /// Normalizes the metric data to ensure it is in a consistent state for analysis.
+    ///
+    /// This includes, but is not limited to, sorting the tags to match the behavior of the normal Datadog metrics
+    /// intake, etc.
+    pub fn normalize(&mut self) {
+        self.context.normalize();
+
+        // Sort values by timestamp, in ascending order, and then merge duplicates.
+        self.values.sort_by(|a, b| a.0.cmp(&b.0));
+        self.values.dedup_by(|a, b| {
+            if a.0 == b.0 {
+                // We merge `a` into `b` if the timestamps are equal, because `a` is the one that gets removed if they match.
+                match (&mut a.1, &mut b.1) {
+                    (MetricValue::Count { value: value_a }, MetricValue::Count { value: value_b }) => *value_b += *value_a,
+                    (MetricValue::Rate { interval: interval_a, value: value_a }, MetricValue::Rate { interval: interval_b, value: value_b }) => {
+                        assert_eq!(*interval_a, *interval_b, "Rate intervals should be identical.");
+                        *value_b += *value_a;
+                    },
+                    (MetricValue::Gauge { value: value_a }, MetricValue::Gauge { value: value_b }) => *value_b = *value_a,
+                    (MetricValue::Sketch { sketch: sketch_a }, MetricValue::Sketch { sketch: sketch_b }) => sketch_b.merge(sketch_a),
+                    _ => panic!("Metric types should be identical."),
+                }
+                true
+            } else {
+                false
+            }
+        });
+    }
+
+    /// Returns the context of the metric.
+    pub fn context(&self) -> &MetricContext {
+        &self.context
     }
 
     /// Returns the values associated with the metric.
     pub fn values(&self) -> &[(u64, MetricValue)] {
         &self.values
     }
+
+    /// Merges the values of another metric into this one.
+    ///
+    /// Values are normalized after merging.
+    pub fn merge_values(&mut self, other: &Metric) {
+        self.values.extend_from_slice(&other.values);
+        self.normalize();
+    }
+
+    /// Anchors the timestamps of metric values.
+    ///
+    /// This will adjust the timestamps such that the first/lowest timestamp is 0, and all other timestamps are offset
+    /// relative to that. For example, with two timestamps -- 10010 and 10030 -- the first timestamp of 10010 would
+    /// become 0, and the second timestamp of 10030 would become 20.
+    pub fn anchor_values(&mut self) {
+        self.normalize();
+
+        let base_offset = if let Some((first_timestamp, _)) = self.values.first() {
+            *first_timestamp
+        } else {
+            return;
+        };
+
+        for (timestamp, _) in &mut self.values {
+            *timestamp -= base_offset;
+        }
+    }
 }
 
 /// A metric value.
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(tag = "mtype")]
 pub enum MetricValue {
     /// A count.
@@ -44,6 +132,22 @@ pub enum MetricValue {
     /// A sketch.
     Sketch { sketch: DDSketch },
 }
+
+impl PartialEq for MetricValue {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (MetricValue::Count { value: value_a }, MetricValue::Count { value: value_b }) => value_a == value_b,
+            (MetricValue::Rate { interval: interval_a, value: value_a }, MetricValue::Rate { interval: interval_b, value: value_b }) => {
+                interval_a == interval_b && value_a == value_b
+            },
+            (MetricValue::Gauge { value: value_a }, MetricValue::Gauge { value: value_b }) => value_a == value_b,
+            (MetricValue::Sketch { sketch: sketch_a }, MetricValue::Sketch { sketch: sketch_b }) => sketch_a == sketch_b,
+            _ => false,
+        }
+    }
+}
+
+impl Eq for MetricValue {}
 
 impl Metric {
     /// Attempts to parse metrics from a series payload.
@@ -92,7 +196,7 @@ impl Metric {
                 }
             }
 
-            metrics.push(Metric { name, tags, values })
+            metrics.push(Metric { context: MetricContext { name, tags }, values })
         }
 
         Ok(metrics)
@@ -119,7 +223,7 @@ impl Metric {
                 values.push((timestamp, MetricValue::Sketch { sketch }));
             }
 
-            metrics.push(Metric { name, tags, values })
+            metrics.push(Metric { context: MetricContext { name, tags }, values })
         }
 
         Ok(metrics)
