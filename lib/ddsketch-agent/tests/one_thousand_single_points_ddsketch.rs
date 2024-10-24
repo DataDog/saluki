9cc1c79c191961bc2bad0fa7dcd2880b55801be6
George Hahn <george.hahn@datadoghq.com>
2024-10-24 13:12:43 -0600

    update mem tests

--- lib/ddsketch-agent/tests/one_thousand_single_points_ddsketch.rs
+++ lib/ddsketch-agent/tests/one_thousand_single_points_ddsketch.rs
@@ -20,23 +20,10 @@ fn test_one_thousand_single_points_ddsketch() {
     insert_single_points(&points);
     let stats = dhat::HeapStats::get();
 
-    // Comments note serialization overhead (see `fn merge_to_dogsketch`)
-    dhat::assert_eq!(stats.total_blocks, 13); // + 14
-    dhat::assert_eq!(stats.total_bytes, 1600); // + 4064
-    dhat::assert_eq!(stats.max_blocks, 4); // + 2
-    dhat::assert_eq!(stats.max_bytes, 864); // + 2048
+    dhat::assert_eq!(stats.total_blocks, 11);
+    dhat::assert_eq!(stats.total_bytes, 1504);
+    dhat::assert_eq!(stats.max_blocks, 2);
+    dhat::assert_eq!(stats.max_bytes, 768);
     dhat::assert_eq!(stats.curr_blocks, 0);
     dhat::assert_eq!(stats.curr_bytes, 0);
-
-    // separate kv list branch shows (this includes serialization):
-    // dhat::assert_eq!(stats.total_blocks, 14);
-    // dhat::assert_eq!(stats.total_bytes, 3736);
-    // dhat::assert_eq!(stats.max_blocks, 4);
-    // dhat::assert_eq!(stats.max_bytes, 2744);
-
-    // and main, without serialization:
-    // dhat::assert_eq!(stats.total_blocks, 6);
-    // dhat::assert_eq!(stats.total_bytes, 2016);
-    // dhat::assert_eq!(stats.max_blocks, 1);
-    // dhat::assert_eq!(stats.max_bytes, 1024);
 }
