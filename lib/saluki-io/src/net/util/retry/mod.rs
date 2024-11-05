d0f955d1da4b44f86884642105bc9a6fa289b295
Toby Lawrence <tobz@users.noreply.github.com>
2024-11-05 12:07:58 -0500

    chore: move more middleware logic into `HttpClient` directly (#298)

--- lib/saluki-io/src/net/util/retry/mod.rs
+++ lib/saluki-io/src/net/util/retry/mod.rs
@@ -8,4 +8,18 @@ mod lifecycle;
 pub use self::lifecycle::StandardHttpRetryLifecycle;
 
 mod policy;
-pub use self::policy::RollingExponentialBackoffRetryPolicy;
+pub use self::policy::{NoopRetryPolicy, RollingExponentialBackoffRetryPolicy};
+
+/// A batteries-included retry policy suitable for HTTP-based clients.
+pub type DefaultHttpRetryPolicy =
+    RollingExponentialBackoffRetryPolicy<StandardHttpClassifier, StandardHttpRetryLifecycle>;
+
+impl DefaultHttpRetryPolicy {
+    /// Creates a new retry policy adapted to HTTP-based clients with the given exponential backoff strategy.
+    ///
+    /// This policy uses the standard HTTP classifier ([`StandardHttpRClassifier`]) and retry lifecycle ([`StandardHttpRetryLifecycle`]).
+    pub fn with_backoff(backoff: ExponentialBackoff) -> Self {
+        RollingExponentialBackoffRetryPolicy::new(StandardHttpClassifier, backoff)
+            .with_retry_lifecycle(StandardHttpRetryLifecycle)
+    }
+}
