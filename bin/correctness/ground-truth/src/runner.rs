829cb4e2cbcf8167364e7f50174540be6bec0e5a
Toby Lawrence <toby@nuclearfurnace.com>
2024-11-07 16:31:20 +0000

    chore: switch admin API and telemetry API ports to be closer together with each other and with Agent ports

--- bin/correctness/ground-truth/src/runner.rs
+++ bin/correctness/ground-truth/src/runner.rs
@@ -89,7 +89,7 @@ impl TestRunner {
             .with_env_var("DD_API_KEY", "dummy-api-key-correctness-testing")
             .with_env_var("DD_ADP_USE_NOOP_WORKLOAD_PROVIDER", "true")
             .with_env_var("DD_TELEMETRY_ENABLED", "true")
-            .with_env_var("DD_PROMETHEUS_LISTEN_ADDR", "tcp://0.0.0.0:6000")
+            .with_env_var("DD_PROMETHEUS_LISTEN_ADDR", "tcp://0.0.0.0:5101")
             .with_exposed_port("tcp", 6000);
 
         group_runner
