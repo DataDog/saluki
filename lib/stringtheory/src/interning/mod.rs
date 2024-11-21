d4e1262098a49e73f7758061b97fb7a7a7010121
Toby Lawrence <toby@nuclearfurnace.com>
2024-11-21 13:47:14 +0000

    chore: coalesce tail entries with spare capacity during reclamation

--- lib/stringtheory/src/interning/mod.rs
+++ lib/stringtheory/src/interning/mod.rs
@@ -246,6 +246,10 @@ impl ReclaimedEntries {
         Some(found_entry)
     }
 
+    fn remove(&mut self, entry: &ReclaimedEntry) {
+        self.0.remove(entry);
+    }
+
     #[cfg(test)]
     fn iter(&self) -> impl Iterator<Item = &ReclaimedEntry> {
         self.0.iter()
