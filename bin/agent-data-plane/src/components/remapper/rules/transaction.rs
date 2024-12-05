c7fa9279188000d896eeb48a00f1b07fb6b5a57c
Toby Lawrence <toby@nuclearfurnace.com>
2024-12-05 15:43:10 -0500

    chore: drop datadog.agent prefix on metrics to allow for easier scraping of internal telemetry

--- bin/agent-data-plane/src/components/remapper/rules/transaction.rs
+++ bin/agent-data-plane/src/components/remapper/rules/transaction.rs
@@ -3,32 +3,26 @@ use crate::components::remapper::RemapperRule;
 pub fn get_transaction_remappings() -> Vec<RemapperRule> {
     vec![
         // Transaction metrics.
+        RemapperRule::by_name("adp.network_http_requests_failed_total", "transactions.dropped")
+            .with_original_tags(["domain", "endpoint"]),
+        RemapperRule::by_name("adp.network_http_requests_success_total", "transactions.success")
+            .with_original_tags(["domain", "endpoint"]),
         RemapperRule::by_name(
-            "datadog.agent.adp.network_http_requests_failed_total",
-            "datadog.agent.transactions.dropped",
-        )
-        .with_original_tags(["domain", "endpoint"]),
-        RemapperRule::by_name(
-            "datadog.agent.adp.network_http_requests_success_total",
-            "datadog.agent.transactions.success",
-        )
-        .with_original_tags(["domain", "endpoint"]),
-        RemapperRule::by_name(
-            "datadog.agent.adp.network_http_requests_success_sent_bytes_total",
-            "datadog.agent.transactions.success_bytes",
+            "adp.network_http_requests_success_sent_bytes_total",
+            "transactions.success_bytes",
         )
         .with_original_tags(["domain", "endpoint"]),
         RemapperRule::by_name_and_tags(
-            "datadog.agent.adp.network_http_requests_errors_total",
+            "adp.network_http_requests_errors_total",
             &["error_type:client_error"],
-            "datadog.agent.transactions.http_errors",
+            "transactions.http_errors",
         )
         .with_original_tags(["domain", "endpoint", "code"])
         .with_remapped_tags([("error_type", "status_code")]),
-        RemapperRule::by_name(
-            "datadog.agent.adp.network_http_requests_errors_total",
-            "datadog.agent.transactions.errors",
-        )
-        .with_original_tags(["domain", "endpoint", "error_type"]),
+        RemapperRule::by_name("adp.network_http_requests_errors_total", "transactions.errors").with_original_tags([
+            "domain",
+            "endpoint",
+            "error_type",
+        ]),
     ]
 }
