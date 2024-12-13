cc7fe1daa8ffdeaa838aa57ba0378d516af2d8c9
Toby Lawrence <toby@nuclearfurnace.com>
2024-12-13 14:39:01 -0500

    chore: add two more internal metrics to remap

--- bin/agent-data-plane/src/components/remapper/rules/aggregation.rs
+++ bin/agent-data-plane/src/components/remapper/rules/aggregation.rs
@@ -2,11 +2,10 @@ use crate::components::remapper::RemapperRule;
 
 pub fn get_aggregation_remappings() -> Vec<RemapperRule> {
     vec![
-        // Aggregation metrics. (placeholder so we can have the below TODO)
-        //
-        // TODO: We need to add two new metrics to the DD Metrics destination to properly capture the intent of
-        // `datadog.agent.aggregator.flush`, which is the number of series/sketches that we managed to successfully
-        // serialize. The metric doesn't concern itself with whether or not those payloads actually make it anywhere,
-        // just if they were sent out of the aggregator and serialized correctly.
+        RemapperRule::by_name_and_tags(
+            "adp.context_resolver_active_contexts",
+            &["resolver_id:dogstatsd"],
+            "aggregator.dogstatsd_contexts",
+        ),
     ]
 }
