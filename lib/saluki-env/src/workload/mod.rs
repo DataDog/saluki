d9aba07753c1f67d59a0f1960ef1cd289329d075
Toby Lawrence <toby@nuclearfurnace.com>
2025-01-23 13:25:40 -0500

    chore: slightly revamp External Data support to expose both pod and container entity ID

--- lib/saluki-env/src/workload/mod.rs
+++ lib/saluki-env/src/workload/mod.rs
@@ -16,6 +16,8 @@ mod entity;
 pub use self::entity::EntityId;
 
 mod external_data;
+pub use self::external_data::ResolvedExternalData;
+
 mod helpers;
 mod metadata;
 pub use self::metadata::{MetadataAction, MetadataOperation};
@@ -35,13 +37,13 @@ pub trait WorkloadProvider {
     /// If no tags can be found for the entity, or at the given cardinality, `None` is returned.
     fn get_tags_for_entity(&self, entity_id: &EntityId, cardinality: OriginTagCardinality) -> Option<SharedTagSet>;
 
-    /// Resolves an entity ID from external data.
+    /// Resolves a raw External Data identifier.
     ///
-    /// External data is free-form string data attached to entities and designed for deferred resolution when they are
+    /// External Data is free-form string data attached to entities and designed for deferred resolution when they are
     /// unable to access their entity ID directly for attachment to telemetry payloads.
     ///
-    /// If the external data is invalid, or no entity ID can be resolved from it, `None` is returned.
-    fn resolve_entity_id_from_external_data(&self, external_data: &str) -> Option<EntityId>;
+    /// If the raw External Data identifier is invalid, or no entities could be resolved to it, `None` is returned.
+    fn resolve_external_data(&self, raw_external_data: &str) -> Option<ResolvedExternalData>;
 }
 
 impl<T> WorkloadProvider for Option<T>
@@ -55,9 +57,9 @@ where
         }
     }
 
-    fn resolve_entity_id_from_external_data(&self, external_data: &str) -> Option<EntityId> {
+    fn resolve_external_data(&self, raw_external_data: &str) -> Option<ResolvedExternalData> {
         match self.as_ref() {
-            Some(provider) => provider.resolve_entity_id_from_external_data(external_data),
+            Some(provider) => provider.resolve_external_data(raw_external_data),
             None => None,
         }
     }
