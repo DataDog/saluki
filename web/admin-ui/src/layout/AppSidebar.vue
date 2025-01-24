62d8ca892e940386d0657fad4d91aae38a1300fe
Toby Lawrence <toby@nuclearfurnace.com>
2025-01-24 14:48:41 -0500

    more tweaking and paring down from the starter project

--- web/admin-ui/src/layout/AppSidebar.vue
+++ web/admin-ui/src/layout/AppSidebar.vue
@@ -3,9 +3,9 @@ import AppMenu from './AppMenu.vue';
 </script>
 
 <template>
-    <div class="layout-sidebar">
-        <app-menu></app-menu>
-    </div>
+  <div class="layout-sidebar">
+    <app-menu></app-menu>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- web/admin-ui/src/layout/composables/layout.js
+++ web/admin-ui/src/layout/composables/layout.js
@@ -1,72 +1,63 @@
 import { computed, reactive } from 'vue';
 
 const layoutConfig = reactive({
-    preset: 'Aura',
-    primary: 'emerald',
-    surface: null,
-    darkTheme: false,
-    menuMode: 'static'
+  darkTheme: false,
+  menuMode: 'static'
 });
 
 const layoutState = reactive({
-    staticMenuDesktopInactive: false,
-    overlayMenuActive: false,
-    profileSidebarVisible: false,
-    configSidebarVisible: false,
-    staticMenuMobileActive: false,
-    menuHoverActive: false,
-    activeMenuItem: null
+  staticMenuDesktopInactive: false,
+  overlayMenuActive: false,
+  profileSidebarVisible: false,
+  configSidebarVisible: false,
+  staticMenuMobileActive: false,
+  menuHoverActive: false,
+  activeMenuItem: null
 });
 
 export function useLayout() {
-    const setActiveMenuItem = (item) => {
-        layoutState.activeMenuItem = item.value || item;
-    };
+  const setActiveMenuItem = (item) => {
+    layoutState.activeMenuItem = item.value || item;
+  };
 
-    const toggleDarkMode = () => {
-        if (!document.startViewTransition) {
-            executeDarkModeToggle();
+  const toggleDarkMode = () => {
+    if (!document.startViewTransition) {
+      executeDarkModeToggle();
 
-            return;
-        }
+      return;
+    }
 
-        document.startViewTransition(() => executeDarkModeToggle(event));
-    };
+    document.startViewTransition(() => executeDarkModeToggle(event));
+  };
 
-    const executeDarkModeToggle = () => {
-        layoutConfig.darkTheme = !layoutConfig.darkTheme;
-        document.documentElement.classList.toggle('app-dark');
-    };
+  const executeDarkModeToggle = () => {
+    layoutConfig.darkTheme = !layoutConfig.darkTheme;
+    document.documentElement.classList.toggle('app-dark');
+  };
 
-    const toggleMenu = () => {
-        if (layoutConfig.menuMode === 'overlay') {
-            layoutState.overlayMenuActive = !layoutState.overlayMenuActive;
-        }
+  const toggleMenu = () => {
+    if (layoutConfig.menuMode === 'overlay') {
+      layoutState.overlayMenuActive = !layoutState.overlayMenuActive;
+    }
 
-        if (window.innerWidth > 991) {
-            layoutState.staticMenuDesktopInactive = !layoutState.staticMenuDesktopInactive;
-        } else {
-            layoutState.staticMenuMobileActive = !layoutState.staticMenuMobileActive;
-        }
-    };
+    if (window.innerWidth > 991) {
+      layoutState.staticMenuDesktopInactive = !layoutState.staticMenuDesktopInactive;
+    } else {
+      layoutState.staticMenuMobileActive = !layoutState.staticMenuMobileActive;
+    }
+  };
 
-    const isSidebarActive = computed(() => layoutState.overlayMenuActive || layoutState.staticMenuMobileActive);
+  const isSidebarActive = computed(() => layoutState.overlayMenuActive || layoutState.staticMenuMobileActive);
 
-    const isDarkTheme = computed(() => layoutConfig.darkTheme);
+  const isDarkTheme = computed(() => layoutConfig.darkTheme);
 
-    const getPrimary = computed(() => layoutConfig.primary);
-
-    const getSurface = computed(() => layoutConfig.surface);
-
-    return {
-        layoutConfig,
-        layoutState,
-        toggleMenu,
-        isSidebarActive,
-        isDarkTheme,
-        getPrimary,
-        getSurface,
-        setActiveMenuItem,
-        toggleDarkMode
-    };
+  return {
+    layoutConfig,
+    layoutState,
+    toggleMenu,
+    isSidebarActive,
+    isDarkTheme,
+    setActiveMenuItem,
+    toggleDarkMode
+  };
 }
