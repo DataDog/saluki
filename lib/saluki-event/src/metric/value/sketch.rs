194e299654331054d1ef45c0c743c3e0e1c78738
Toby Lawrence <tobz@users.noreply.github.com>
2025-01-28 11:44:41 -0500

    fix: merge gauges properly and avoid dropping existing points (#444)

--- lib/saluki-event/src/metric/value/sketch.rs
+++ lib/saluki-event/src/metric/value/sketch.rs
@@ -1,4 +1,4 @@
-use std::num::NonZeroU64;
+use std::{fmt, num::NonZeroU64};
 
 use ddsketch_agent::DDSketch;
 
@@ -115,6 +115,15 @@ impl From<(u64, f64)> for SketchPoints {
     }
 }
 
+impl<const N: usize> From<(u64, [f64; N])> for SketchPoints {
+    fn from((ts, values): (u64, [f64; N])) -> Self {
+        let mut sketch = DDSketch::default();
+        sketch.insert_many(&values[..]);
+
+        Self(TimestampedValue::from((ts, sketch)).into())
+    }
+}
+
 impl<'a> From<(u64, &'a [f64])> for SketchPoints {
     fn from((ts, values): (u64, &'a [f64])) -> Self {
         let mut sketch = DDSketch::default();
@@ -129,6 +138,17 @@ impl<'a> From<&'a [(u64, &'a [f64])]> for SketchPoints {
         Self(TimestampedValues::from(values.iter().map(|(ts, values)| {
             let mut sketch = DDSketch::default();
             sketch.insert_many(values);
+
+            (*ts, sketch)
+        })))
+    }
+}
+
+impl<const N: usize> From<[(u64, f64); N]> for SketchPoints {
+    fn from(values: [(u64, f64); N]) -> Self {
+        Self(TimestampedValues::from(values.iter().map(|(ts, value)| {
+            let mut sketch = DDSketch::default();
+            sketch.insert(*value);
 
             (*ts, sketch)
         })))
@@ -165,6 +185,32 @@ impl<'a> IntoIterator for &'a SketchPoints {
         SketchesIterRef {
             inner: self.0.values.iter(),
         }
+    }
+}
+
+impl fmt::Display for SketchPoints {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+        for (i, point) in self.0.values.iter().enumerate() {
+            if i > 0 {
+                write!(f, ",")?;
+            }
+
+            let ts = point.timestamp.map(|ts| ts.get()).unwrap_or_default();
+            let sketch = &point.value;
+            write!(
+                f,
+                "({}, {{cnt={} min={} max={} avg={} sum={} bin_count={}}})",
+                ts,
+                sketch.count(),
+                sketch.min().unwrap_or(0.0),
+                sketch.max().unwrap_or(0.0),
+                sketch.avg().unwrap_or(0.0),
+                sketch.sum().unwrap_or(0.0),
+                sketch.bin_count(),
+            )?;
+        }
+        write!(f, "]")
     }
 }
 
