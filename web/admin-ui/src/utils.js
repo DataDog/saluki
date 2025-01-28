4e29e259247e36f8fb76f46be8de12d593723a82
Toby Lawrence <toby@nuclearfurnace.com>
2025-01-28 09:37:06 -0500

    more tweaks

--- web/admin-ui/src/utils.js
+++ web/admin-ui/src/utils.js
@@ -17,3 +17,35 @@ export function bytesToHumanFriendly(bytes) {
 
   return bytes.toFixed(decimalPoints) + ' ' + units[u];
 }
+
+export function secondsToHumanFriendly(seconds) {
+  const divisors = [60, 60, 24, 365];
+  const units = ['s', 'm', 'h', 'd', 'y'];
+
+  if (Math.abs(seconds) < 60) {
+    return seconds + 's';
+  }
+
+  // Iterate over the input value (base), continuously moduloing/dividing it in order to break it down into the
+  // different unit components.
+  let i = 0;
+  let base = seconds;
+  let parts = [];
+
+  do {
+    const remainder = base % divisors[i];
+    base = (base - remainder) / divisors[i];
+
+    parts.unshift([remainder, units[i]]);
+
+    i += 1;
+  } while (base > 1 && i < divisors.length);
+
+  // Reassemble each unit component to get our final duration string.
+  let result = '';
+  for (const [value, unit] of parts) {
+    result += `${value}${unit} `;
+  }
+
+  return result;
+}
