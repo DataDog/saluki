f4fc01d278da3c036599cf2f3b60b544eafb1e98
Raymond Zhao <35050708+rayz@users.noreply.github.com>
2025-02-18 12:25:02 -0500

    enhancement: add support for attaching host tags for a specified duration on startup (#494)
    ## Summary
    <!-- Please provide a brief summary about what this PR does.
    This should help the reviewers give feedback faster and with higher
    quality. -->
    This pr introduces a new transform that enriches metrics on start up for
    `expected_tags_duration` seconds. After the time has elapsed, the
    transform does notthing.
    
    The tags are retrieved by calling the `v1/grpc/host_tags` grpc endpoint
    using `RemoteAgentClient`.
    ## Change Type
    - [ ] Bug fix
    - [x] New feature
    - [ ] Non-functional (chore, refactoring, docs)
    - [ ] Performance
    
    
    ## How did you test this PR?
    <!-- Please how you tested these changes here -->
    1. Set `expected_tags_duration` to `120`
    2. Set some host tags in `datadog.yaml`
    3. Run adp and send some metrics
    4. Observe that the metrics include the host tags aswell as the existing
    user added tags
    5. Send metrics after `expected_tags_duration` has elapsed
    6. The metrics will still have tags showing on Datadog
    ## References
    
    <!-- Please list any issues closed by this PR. -->
    - Closes: #388
    <!--
    - Closes: <issue link>
    -->
    
    <!-- Any other issues or PRs relevant to this PR? Feel free to list them
    here. -->
    
    ---------
    
    Signed-off-by: Raymond Zhao <raymond.zhao@datadoghq.com>

--- lib/saluki-components/src/transforms/mod.rs
+++ lib/saluki-components/src/transforms/mod.rs
@@ -12,5 +12,8 @@ pub use self::host_enrichment::HostEnrichmentConfiguration;
 mod dogstatsd_prefix_filter;
 pub use self::dogstatsd_prefix_filter::DogstatsDPrefixFilterConfiguration;
 
+mod host_tags;
+pub use self::host_tags::HostTagsConfiguration;
+
 mod dogstatsd_mapper;
 pub use self::dogstatsd_mapper::DogstatsDMapperConfiguration;
