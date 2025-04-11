275dad9bff70dc34136f89fd7c8805839dfa5c66
Toby Lawrence <toby@nuclearfurnace.com>
2025-04-11 08:59:20 -0400

    enhancement(context): switch to noop hasher for maps/sets with prehashed keys

--- lib/saluki-common/src/hash.rs
+++ lib/saluki-common/src/hash.rs
@@ -1,5 +1,5 @@
 use std::{
-    hash::{BuildHasher as _, Hasher as _},
+    hash::{BuildHasher, Hasher},
     sync::LazyLock,
 };
 
@@ -19,6 +19,48 @@ pub type FastBuildHasher = foldhash::quality::RandomState;
 // Single global instance of the hasher state since we need a consistently-seeded state for `hash_single_fast` to
 // consistently hash things across the application.
 static BUILD_HASHER: LazyLock<FastBuildHasher> = LazyLock::new(foldhash::quality::RandomState::default);
+
+/// A no-op hasher that writes `u64` values directly to the internal state.
+///
+/// In some cases, pre-hashed values (`u64`) may be used as the key to a hash table or similar data structure. In those
+/// cases, re-hashing the key each time is unnecessary and potentially even undesirable.
+///
+/// `NoopU64Hasher` is a hash implementation that simply forwards `u64` values to the internal state and uses that as
+/// the final hashed value. It can used to hash a `u64` value directly, or to hash a value that wraps a `u64` value,
+/// such as a newtype (e.g., `struct HashKey(u64)`).
+///
+/// # Behavior
+///
+/// `NoopU64Hasher` stores a single `u64` value internally. The last value written via `write_u64` is the final hash
+/// value. Writing any other value type to the hasher will panic.
+#[derive(Default)]
+pub struct NoopU64Hasher(u64);
+
+impl Hasher for NoopU64Hasher {
+    fn finish(&self) -> u64 {
+        self.0
+    }
+
+    fn write_u64(&mut self, i: u64) {
+        self.0 = i;
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        panic!("non-u64 value written to NoopU64Hasher: {:?}", bytes);
+    }
+}
+
+/// A [`BuildHasher`][std::hash::BuildHasher] implementation for [`NoopU64Hasher`].
+#[derive(Clone, Default)]
+pub struct NoopU64BuildHasher;
+
+impl BuildHasher for NoopU64BuildHasher {
+    type Hasher = NoopU64Hasher;
+
+    fn build_hasher(&self) -> Self::Hasher {
+        NoopU64Hasher::default()
+    }
+}
 
 /// Returns a fresh `FastBuildHasher` instance.
 ///
