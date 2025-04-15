3bbc318f4d5ff110de02e47da8fc433beb12255f
Toby Lawrence <toby@nuclearfurnace.com>
2025-04-15 10:01:22 -0400

    update prefix for ADP internal metrics + adjust remapper rules

--- bin/agent-data-plane/src/components/remapper/rules/transaction.rs
+++ bin/agent-data-plane/src/components/remapper/rules/transaction.rs
@@ -3,22 +3,22 @@ use crate::components::remapper::RemapperRule;
 pub fn get_transaction_remappings() -> Vec<RemapperRule> {
     vec![
         // Transaction metrics.
-        RemapperRule::by_name("adp.network_http_requests_failed_total", "transactions.dropped")
+        RemapperRule::by_name("datadog.agent.adp.network_http_requests_failed_total", "transactions.dropped")
             .with_original_tags(["domain", "endpoint"]),
-        RemapperRule::by_name("adp.network_http_requests_success_total", "transactions.success")
+        RemapperRule::by_name("datadog.agent.adp.network_http_requests_success_total", "transactions.success")
             .with_original_tags(["domain", "endpoint"]),
         RemapperRule::by_name(
-            "adp.network_http_requests_success_sent_bytes_total",
+            "datadog.agent.adp.network_http_requests_success_sent_bytes_total",
             "transactions.success_bytes",
         )
         .with_original_tags(["domain", "endpoint"]),
         RemapperRule::by_name_and_tags(
-            "adp.network_http_requests_errors_total",
+            "datadog.agent.adp.network_http_requests_errors_total",
             &["error_type:client_error"],
             "transactions.http_errors",
         )
         .with_original_tags(["domain", "endpoint", "code"]),
-        RemapperRule::by_name("adp.network_http_requests_errors_total", "transactions.errors").with_original_tags([
+        RemapperRule::by_name("datadog.agent.adp.network_http_requests_errors_total", "transactions.errors").with_original_tags([
             "domain",
             "endpoint",
             "error_type",
