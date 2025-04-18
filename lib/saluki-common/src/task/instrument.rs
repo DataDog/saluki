50391297a4fc1a43339f8d5f70da1a89bdf3e4d2
Toby Lawrence <toby@nuclearfurnace.com>
2025-04-18 12:53:44 -0400

    remove extraneous task registry + add docs

--- lib/saluki-common/src/task/instrument.rs
+++ lib/saluki-common/src/task/instrument.rs
@@ -1,21 +1,16 @@
 use std::{
     future::Future,
     pin::Pin,
-    sync::{Arc, OnceLock},
     task::{Context, Poll},
 };
 
-use pin_project::{pin_project, pinned_drop};
+use pin_project::pin_project;
 use saluki_metrics::static_metrics;
-
-use crate::collections::FastConcurrentHashMap;
-
-static TASK_INSTRUMENTATION_REGISTRY: OnceLock<InstrumentationRegistry> = OnceLock::new();
 
 static_metrics!(
    name => Telemetry,
    prefix => runtime_task,
-   labels => [task_name: Arc<str>],
+   labels => [task_name: String],
    metrics => [
        counter(poll_count),
        histogram(poll_duration_seconds),
@@ -25,7 +20,15 @@ static_metrics!(
 /// Helper trait for instrumenting futures that are run as asynchronous tasks.
 pub trait TaskInstrument {
     /// Instruments the future, tracking task-specific metrics about its execution.
-    fn with_task_instrumentation<S: AsRef<str>>(self, task_name: S) -> InstrumentedTask<Self>
+    ///
+    /// Whenever the resulting future is polled, an internal metric (`runtime_task.poll_duration_seconds`) will be
+    /// updated with the duration of the poll operation, in seconds. This metric will be tagged with the task name
+    /// provided here (as `task_name:<task name>`).
+    ///
+    /// In general, a unique task name should be provided where possible. If multiple tasks share the same task name,
+    /// they will all update the same metric, which will simply influence the resulting percentiles and make it more
+    /// difficult to isolate outlier poll durations.
+    fn with_task_instrumentation(self, task_name: String) -> InstrumentedTask<Self>
     where
         Self: Sized;
 }
@@ -34,14 +37,19 @@ impl<F> TaskInstrument for F
 where
     F: Future + Send + 'static,
 {
-    fn with_task_instrumentation<S: AsRef<str>>(self, task_name: S) -> InstrumentedTask<Self> {
-        InstrumentationRegistry::global().instrument_task(Arc::from(task_name.as_ref()), self)
+    fn with_task_instrumentation(self, task_name: String) -> InstrumentedTask<Self> {
+        InstrumentedTask {
+            telemetry: Telemetry::new(task_name),
+            inner: self,
+        }
     }
 }
 
-#[pin_project(PinnedDrop)]
+/// An instrumented task future.
+///
+/// This wraps a `Future` and emits telemetry about the duration of each poll operation.
+#[pin_project]
 pub struct InstrumentedTask<F> {
-    task_name: Arc<str>,
     telemetry: Telemetry,
 
     #[pin]
@@ -61,53 +69,8 @@ where
         let result = this.inner.poll(cx);
         let poll_duration = poll_start.elapsed();
 
-        this.telemetry.poll_count.increment(1);
         this.telemetry.poll_duration_seconds.record(poll_duration.as_secs_f64());
 
         result
-    }
-}
-
-#[pinned_drop]
-impl<F> PinnedDrop for InstrumentedTask<F> {
-    fn drop(self: Pin<&mut Self>) {
-        InstrumentationRegistry::global().remove_task(&self.task_name);
-    }
-}
-
-struct InstrumentationRegistry {
-    tasks: FastConcurrentHashMap<Arc<str>, Telemetry>,
-}
-
-impl InstrumentationRegistry {
-    fn new() -> Self {
-        Self {
-            tasks: FastConcurrentHashMap::default(),
-        }
-    }
-
-    /// Returns a reference to the global instrumentation registry.
-    pub fn global() -> &'static Self {
-        TASK_INSTRUMENTATION_REGISTRY.get_or_init(InstrumentationRegistry::new)
-    }
-
-    fn instrument_task<F>(&self, task_name: Arc<str>, f: F) -> InstrumentedTask<F>
-    where
-        F: Future + Send + 'static,
-    {
-        let tasks_guard = self.tasks.pin();
-        let telemetry = tasks_guard
-            .get_or_insert_with(task_name.clone(), || Telemetry::new(task_name.clone()))
-            .clone();
-
-        InstrumentedTask {
-            task_name,
-            telemetry,
-            inner: f,
-        }
-    }
-
-    fn remove_task(&self, task_name: &Arc<str>) {
-        self.tasks.pin().remove(task_name);
     }
 }
