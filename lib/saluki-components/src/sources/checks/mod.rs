5c8e4e9992d381ae48c0dad3cd0a792b2a966dff
Gustavo Caso <gustavo.caso@datadoghq.com>
2025-04-30 13:49:21 +0200

    sets autodiscovey provider

--- lib/saluki-components/src/sources/checks/mod.rs
+++ lib/saluki-components/src/sources/checks/mod.rs
@@ -1,7 +1,7 @@
 /// Checks source.
 ///
 /// Listen to Autodiscovery events, schedule checks and emit results.
-use std::sync::LazyLock;
+use std::{sync::Arc, sync::LazyLock};
 
 use async_trait::async_trait;
 use memory_accounting::{MemoryBounds, MemoryBoundsBuilder};
@@ -10,18 +10,32 @@ use saluki_core::{
     components::{sources::*, ComponentContext},
     topology::OutputDefinition,
 };
+use saluki_env::autodiscovery::AutodiscoveryProvider;
 use saluki_error::GenericError;
 use saluki_event::DataType;
 use serde::Deserialize;
 
 /// Configuration for the checks source.
-#[derive(Debug, Deserialize)]
-pub struct ChecksConfiguration {}
+#[derive(Deserialize)]
+pub struct ChecksConfiguration {
+    /// Autodiscovery provider to use.
+    #[serde(skip)]
+    autodiscovery_provider: Option<Arc<dyn AutodiscoveryProvider + Send + Sync>>,
+}
 
 impl ChecksConfiguration {
     /// Creates a new `ChecksConfiguration` from the given configuration.
     pub fn from_configuration(config: &GenericConfiguration) -> Result<Self, GenericError> {
         Ok(config.as_typed()?)
+    }
+
+    /// Sets the autodiscovery provider to use.
+    pub fn with_autodiscovery_provider<A>(mut self, provider: A) -> Self
+    where
+        A: AutodiscoveryProvider + Send + Sync + 'static,
+    {
+        self.autodiscovery_provider = Some(Arc::new(provider));
+        self
     }
 }
 
