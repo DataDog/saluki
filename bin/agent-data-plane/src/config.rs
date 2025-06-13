41d11b62c88c7d4eeff8875767ca7a66b2417b68
Brianna Wang <brianna.wang@datadoghq.com>
2025-06-13 13:26:04 -0400

    cleaned up documentation and style edits

--- bin/agent-data-plane/src/config.rs
+++ bin/agent-data-plane/src/config.rs
@@ -1,30 +1,26 @@
 use std::path::PathBuf;
 
-use clap::{ArgAction, Args, Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(about)]
 pub struct Cli {
-    /// Enable verbose output. (Specify twice for more verbosity.)
-    #[arg(global = true, short = 'v', long, action = ArgAction::Count, default_value_t = 0)]
-    verbose: u8,
-
     /// Subcommand to run.    
     #[command(subcommand)]
-    pub action: Option<Action>, // Makes subcommands optional
+    pub action: Option<Action>,
 }
 
 #[derive(Subcommand)]
 pub enum Action {
-    /// Run subcommand for the ADP binary
+    /// Runs the data plane.
     #[command(name = "run")]
     Run(RunConfig),
 }
 
-/// Run subcommand configuration
+/// Run subcommand configuration.
 #[derive(Args, Debug)]
 pub struct RunConfig {
-    /// Path to the configuration file
+    /// Path to the configuration file.
     #[arg(short = 'c', long = "config", default_value = "/etc/datadog-agent/datadog.yaml")]
     pub config: PathBuf,
 }
