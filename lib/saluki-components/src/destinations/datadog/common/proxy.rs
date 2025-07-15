a88438df0c26772073d982c8b947f95c4849a89f
raymond zhao <raymond.zhao@datadoghq.com>
2025-07-14 21:44:40 -0400

    exact match

--- lib/saluki-components/src/destinations/datadog/common/proxy.rs
+++ lib/saluki-components/src/destinations/datadog/common/proxy.rs
@@ -1,14 +1,10 @@
-use std::net::IpAddr;
-use std::str::FromStr;
 use std::sync::Arc;
 
 use headers::Authorization;
 use hyper_http_proxy::{Custom, Intercept, Proxy};
-use ipnet::IpNet;
 use saluki_error::GenericError;
 use serde::Deserialize;
 use url::Url;
-use wildcard::Wildcard;
 
 #[derive(Clone, Deserialize)]
 pub struct ProxyConfiguration {
@@ -21,28 +17,8 @@ pub struct ProxyConfiguration {
     https_server: Option<String>,
 
     /// Optional list of hosts or CIDR ranges to bypass the proxy.
-    #[serde(default)]
+    #[serde(rename = "proxy_no_proxy")]
     no_proxy: Option<Vec<String>>,
-}
-
-enum Bypass {
-    Domain(String),
-    IpNet(IpNet),
-}
-
-impl Bypass {
-    fn matches(&self, host: &str) -> bool {
-        match self {
-            Bypass::Domain(pattern) => Wildcard::new(pattern.as_bytes()).map_or(false, |w| w.is_match(host.as_bytes())),
-            Bypass::IpNet(n) => {
-                if let Ok(addr) = host.parse::<IpAddr>() {
-                    n.contains(&addr)
-                } else {
-                    false
-                }
-            }
-        }
-    }
 }
 
 impl ProxyConfiguration {
@@ -52,48 +28,30 @@ impl ProxyConfiguration {
     ///
     /// If the configured proxy URLs are invalid, an error is returned.
     pub fn build(&self) -> Result<Vec<Proxy>, GenericError> {
-        let bypass_rules: Arc<Vec<Bypass>> = Arc::new(
-            self.no_proxy
-                .as_deref()
-                .unwrap_or_default()
-                .iter()
-                .map(|p| {
-                    if let Ok(net) = IpNet::from_str(p) {
-                        Bypass::IpNet(net)
-                    } else {
-                        let pattern = if p.starts_with('.') {
-                            format!("*{}", p)
-                        } else {
-                            p.to_string()
-                        };
-                        Bypass::Domain(pattern)
-                    }
-                })
-                .collect(),
-        );
+        let no_proxy_hosts: Arc<Vec<String>> = Arc::new(self.no_proxy.clone().unwrap_or_default());
 
         let mut proxies = Vec::new();
         if let Some(url) = &self.https_server {
-            proxies.push(new_proxy(url, Intercept::Https, bypass_rules.clone())?);
+            proxies.push(new_proxy(url, Intercept::Https, no_proxy_hosts.clone())?);
         }
         if let Some(url) = &self.http_server {
-            proxies.push(new_proxy(url, Intercept::Http, bypass_rules)?);
+            proxies.push(new_proxy(url, Intercept::Http, no_proxy_hosts)?);
         }
         Ok(proxies)
     }
 }
 
 fn new_proxy(
-    proxy_url: &str, intercept_scheme: Intercept, bypass_rules: Arc<Vec<Bypass>>,
+    proxy_url: &str, intercept_scheme: Intercept, no_proxy_hosts: Arc<Vec<String>>,
 ) -> Result<Proxy, GenericError> {
     let url = Url::parse(proxy_url)?;
 
-    let intercept = if bypass_rules.is_empty() {
+    let intercept = if no_proxy_hosts.is_empty() {
         intercept_scheme
     } else {
         let interceptor = move |_scheme: Option<&str>, host: Option<&str>, _port: Option<u16>| {
             if let Some(host) = host {
-                if bypass_rules.iter().any(|r| r.matches(host)) {
+                if no_proxy_hosts.iter().any(|r| r == host) {
                     return false; // Bypass the proxy.
                 }
             }
