59b10afe906757a075e30ceb1947196a10de290c
Toby Lawrence <tobz@users.noreply.github.com>
2025-07-21 14:16:25 -0400

    enhancement(datadog): add Datadog Events encoder (#814)
    ## Summary
    
    This PR adds a basic `Encoder` implementation for Datadog Events.
    
    We can't _do_ anything with it yet as we lack a forwarder implementation to use with it. That will be handled in a follow-up PR.
    
    ## Change Type
    
    - [ ] Bug fix
    - [x] New feature
    - [ ] Non-functional (chore, refactoring, docs)
    - [ ] Performance
    
    ## How did you test this PR?
    
    N/A
    
    ## References
    
    AGTMETRICS-233

--- lib/saluki-components/src/destinations/datadog/mod.rs
+++ lib/saluki-components/src/destinations/datadog/mod.rs
@@ -1,7 +1,10 @@
-pub mod common;
+//! Datadog-specific destinations and helper functions.
 
-pub mod events;
-pub use self::events::DatadogEventsConfiguration;
+mod common;
+pub use self::common::{io::RB_BUFFER_CHUNK_SIZE, request_builder::RequestBuilder, telemetry::ComponentTelemetry};
+
+mod events;
+pub use self::events::{DatadogEventsConfiguration, EventsEndpointEncoder};
 
 mod metrics;
 pub use self::metrics::DatadogMetricsConfiguration;
