b15a18f93536bd79a34a2326ee24bbb4c0697188
Brianna Wang <brianna.wang@datadoghq.com>
2025-07-23 15:22:46 -0400

    added api call to dogstatsd subcommand code

--- bin/agent-data-plane/src/cli/dogstatsd.rs
+++ bin/agent-data-plane/src/cli/dogstatsd.rs
@@ -5,12 +5,17 @@ use crate::config::DogstatsdConfig;
 pub async fn handle_dogstatsd_subcommand(config: DogstatsdConfig) {
     match config {
         DogstatsdConfig::Stats => {
-            handle_dogstatsd_stats().await;
+            let client = reqwest::Client::builder()
+                .danger_accept_invalid_certs(true)
+                .build()
+                .unwrap();
+            handle_dogstatsd_stats(client).await;
         }
     }
 }
 
-async fn handle_dogstatsd_stats() {
+async fn handle_dogstatsd_stats(client: reqwest::Client) {
     info!("DogStatsD stats");
-    // TODO: call API endpoint for dogstatsd-stats
+    let response = client.get("https://localhost:5101/dogstatsd/stats").send().await;
+    println!("response: {:?}", response);
 }
