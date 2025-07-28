8be0471c0ca3a02db05fa65110a401d11e85c7c1
Brianna Wang <brianna.wang@datadoghq.com>
2025-07-28 10:41:23 -0400

    removed datadog folder and refactored

--- lib/saluki-components/src/destinations/datadog/mod.rs
+++
@@ -1,2 +0,0 @@
-mod dsd_stats;
-pub use self::dsd_stats::DogStatsDStatisticsConfiguration;
--- lib/saluki-components/src/destinations/mod.rs
+++ lib/saluki-components/src/destinations/mod.rs
@@ -3,8 +3,8 @@
 mod blackhole;
 pub use self::blackhole::BlackholeConfiguration;
 
-mod datadog;
-pub use self::datadog::DogStatsDStatisticsConfiguration;
+mod dsd_stats;
+pub use self::dsd_stats::DogStatsDStatisticsConfiguration;
 
 mod prometheus;
 pub use self::prometheus::PrometheusConfiguration;
