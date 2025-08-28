e971f85838c3397e465e17176ff79d03659366fc
Toby Lawrence <tobz@users.noreply.github.com>
2025-08-28 16:46:28 -0400

    chore(deps): deps update (#885)

--- lib/saluki-io/src/net/unix/linux.rs
+++ lib/saluki-io/src/net/unix/linux.rs
@@ -1,7 +1,7 @@
-use std::{io, mem};
+use std::io;
 
 use bytes::BufMut;
-use socket2::{SockAddr, SockRef};
+use socket2::{MaybeUninitSlice, MsgHdrMut, SockAddr, SockAddrStorage, SockRef};
 
 use super::ancillary::{ControlMessage, SocketCredentialsAncillaryData};
 use crate::net::addr::{ConnectionAddress, ProcessCredentials};
@@ -15,7 +15,7 @@ pub fn enable_uds_socket_credentials<'sock, S>(socket: &'sock S) -> io::Result<(
 where
     SockRef<'sock>: From<&'sock S>,
 {
-    let sock_ref = socket2::SockRef::from(socket);
+    let sock_ref = SockRef::from(socket);
     sock_ref.set_passcred(true)
 }
 
@@ -23,26 +23,23 @@ pub(super) fn uds_recvmsg<'sock, S, B: BufMut>(socket: &'sock S, buf: &mut B) ->
 where
     SockRef<'sock>: From<&'sock S>,
 {
-    let sock_ref = socket2::SockRef::from(socket);
+    let sock_ref = SockRef::from(socket);
 
     // Create the message header struct that will be populated by the call to `recvmsg`, which includes the peer
     // address, message data, and any ancillary (out-of-band) data.
     //
     // SAFETY: We're allocating `sockaddr_storage`, which is always large enough to hold any address family's socket
     // address structure.
-    //
-    // TODO: We could probably scope this down to `sockaddr_un`, to do a smaller stack allocation... but I need to
-    // research more if that's the right choice in all cases for a Unix domain socket.
-    let sock_storage: libc::sockaddr_storage = unsafe { mem::zeroed() };
-    let sock_storage_len = mem::size_of_val(&sock_storage) as libc::socklen_t;
+    let sock_storage = SockAddrStorage::zeroed();
+    let sock_storage_len = sock_storage.size_of();
     let mut sock_addr = unsafe { SockAddr::new(sock_storage, sock_storage_len) };
 
     let mut ancillary_data = SocketCredentialsAncillaryData::new();
 
-    let data_buf = unsafe { socket2::MaybeUninitSlice::new(buf.chunk_mut().as_uninit_slice_mut()) };
+    let data_buf = unsafe { MaybeUninitSlice::new(buf.chunk_mut().as_uninit_slice_mut()) };
     let mut data_bufs = [data_buf];
 
-    let mut msg_hdr = socket2::MsgHdrMut::new()
+    let mut msg_hdr = MsgHdrMut::new()
         .with_addr(&mut sock_addr)
         .with_buffers(&mut data_bufs)
         .with_control(ancillary_data.as_mut_uninit());
