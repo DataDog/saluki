d53ba2e860ab2bcbe43836878cf048ee6d660fb1
Fabrice Buoro <fabrice.buoro@datadoghq.com>
2025-09-02 14:14:18 +0200

    Implement 'headers'. Moved ExecutionContext around.

--- lib/saluki-components/src/sources/checks/builder/python/execution_context.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashMap;
-
-// Cache execution information from datadog agent for Python checks
-#[derive(Clone)]
-pub struct ExecutionContext {
-    pub hostname: String,
-    pub http_headers: HashMap<String, String>,
-    pub tracemalloc_enabled: bool,
-}
