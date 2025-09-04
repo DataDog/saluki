a45169183020e470d76b0faf55aaabba1d31cfbd
Fabrice Buoro <fabrice.buoro@datadoghq.com>
2025-09-04 23:07:36 +0200

    Address review comments: inline doc + naming convention

--- lib/saluki-components/src/sources/checks/execution_context.rs
+++ lib/saluki-components/src/sources/checks/execution_context.rs
@@ -6,6 +6,9 @@ use saluki_metadata;
 use tracing::warn;
 
 /// Global/shared configuration for checks.
+///
+/// This provides information to checks, either from the configuration or computed from the EnvironmentProvider if one
+/// is provided.
 #[derive(Clone)]
 pub struct ExecutionContext {
     configuration: GenericConfiguration,
@@ -34,6 +37,10 @@ impl ExecutionContext {
         }
     }
 
+    /// Create an `ExecutionContext` from an `EnvironmentProvider`.
+    ///
+    /// The `EnvironmentProvider` is used to compute information from the environment, which are cached into the
+    /// `ExecutionContext`.
     pub async fn from_environment_provider<E>(configuration: GenericConfiguration, environment_provider: &E) -> Self
     where
         E: EnvironmentProvider,
@@ -51,22 +58,28 @@ impl ExecutionContext {
         }
     }
 
+    /// Get a reference to the configuration used to create this `ExecutionContext`.
     pub fn configuration(&self) -> &GenericConfiguration {
         &self.configuration
     }
 
+    /// Get the hostname.
+    ///
+    /// Computed from the `EnvironmentProvider` if one has been provided to the `ExecutionContext` constructor.
     pub fn hostname(&self) -> &str {
         &self.hostname
     }
 
+    /// Override the hostname.
     #[allow(dead_code)]
-    pub fn set_hostname<S: AsRef<str>>(self, hostname: S) -> Self {
-        ExecutionContext {
+    pub fn with_hostname<S: AsRef<str>>(self, hostname: S) -> Self {
+        Self {
             hostname: hostname.as_ref().to_string(),
             ..self
         }
     }
 
+    /// Get a reference to the HTTP headers.
     pub fn http_headers(&self) -> &HashMap<String, String> {
         &self.http_headers
     }
