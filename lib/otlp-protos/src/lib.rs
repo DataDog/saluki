ee83b48b0a2d121e140573b04bceb33856a268ed
Toby Lawrence <toby@nuclearfurnace.com>
2025-09-25 16:12:02 -0400

    allow dead codeeeeee

--- lib/otlp-protos/src/lib.rs
+++ lib/otlp-protos/src/lib.rs
@@ -1,8 +1,10 @@
 //! OpenTelemetry Protocol Buffers definitions.
 #![deny(warnings)]
+#![allow(dead_code)]
 #![allow(clippy::enum_variant_names)]
 #![allow(clippy::doc_overindented_list_items)]
 #![allow(rustdoc::invalid_html_tags)]
+
 mod otlp_include {
     include!(concat!(env!("OUT_DIR"), "/otlp.mod.rs"));
 }
