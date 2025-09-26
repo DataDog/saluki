e4f14201c7df5fbd62c2116301433478c31f6e91
andrewqian2001datadog <andrew.qian@datadoghq.com>
2025-09-26 13:22:45 -0400

    WIP

--- lib/saluki-components/src/common/datadog/middleware.rs
+++ lib/saluki-components/src/common/datadog/middleware.rs
@@ -2,6 +2,8 @@ use http::{
     uri::{Authority, Scheme},
     HeaderName, HeaderValue, Request, Uri,
 };
+use tracing::info;
+use std::str::FromStr as _;
 
 use super::endpoints::ResolvedEndpoint;
 
@@ -20,14 +22,41 @@ pub fn for_resolved_endpoint<B>(mut endpoint: ResolvedEndpoint) -> impl FnMut(Re
         Scheme::try_from(endpoint.endpoint().scheme()).expect("should not fail to construct new endpoint scheme");
     move |mut request| {
         // Build an updated URI by taking the endpoint URL and slapping the request's URI path on the end of it.
+        let path_and_query = request
+            .uri()
+            .path_and_query()
+            .expect("request path must exist")
+            .clone();
+        info!("TEST1 path_and_query {:?}", path_and_query);
+        // For logs, override to logs intake domain.
+        let authority = if path_and_query.as_str().starts_with("/api/v2/logs") {
+            // Attempt to derive the site from the endpoint host in the form "<version>.agent.<site>".
+            // If the pattern isn't present, fall back to the default authority.
+            let base_host = endpoint
+                .endpoint()
+                .host_str()
+                .unwrap_or("");
+            if let Some(idx) = base_host.find(".agent.") {
+                let site = &base_host[idx + ".agent.".len()..];
+                let logs_host = format!("agent-http-intake.logs.{}", site);
+                Authority::from_str(&logs_host).unwrap_or_else(|_| new_uri_authority.clone())
+            } else {
+                new_uri_authority.clone()
+            }
+        } else {
+            new_uri_authority.clone()
+        };
+
         let new_uri = Uri::builder()
             .scheme(new_uri_scheme.clone())
-            .authority(new_uri_authority.clone())
-            .path_and_query(request.uri().path_and_query().expect("request path must exist").clone())
+            .authority(authority)
+            .path_and_query(path_and_query)
             .build()
             .expect("should not fail to construct new URI");
+        info!("TEST2 new_uri {:?}", new_uri);
         let api_key = endpoint.api_key();
         let api_key_value = HeaderValue::from_str(api_key).expect("should not fail to construct API key header value");
+        info!("TEST3 api_key_value {:?}", api_key_value);
         *request.uri_mut() = new_uri;
 
         // Add the API key as a header.
