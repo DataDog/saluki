2e5229ed335c36322ac88eda6bc63c6f05b469d5
raymond zhao <raymond.zhao@datadoghq.com>
2025-10-14 10:37:05 -0400

    Add agent conf location for macos

--- bin/agent-data-plane/src/internal/platform/mod.rs
+++ bin/agent-data-plane/src/internal/platform/mod.rs
@@ -6,5 +6,11 @@ mod linux_impl;
 #[cfg(target_os = "linux")]
 pub use self::linux_impl::*;
 
+#[cfg(target_os = "macos")]
+mod macos_impl;
+
+#[cfg(target_os = "macos")]
+pub use self::macos_impl::*;
+
 /// Prefix for all environment variables used by the Datadog Agent.
 pub const DATADOG_AGENT_ENV_VAR_PREFIX: &str = "DD";
