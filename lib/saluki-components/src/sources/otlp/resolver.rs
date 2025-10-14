9298d84b2ff2c65bb73d69d58813363dadbb73f6
Raymond Zhao <35050708+rayz@users.noreply.github.com>
2025-10-14 15:46:17 -0400

    enhancement: add OTLP origin detection (#967)
    ## Summary
    <!-- Please provide a brief summary about what this PR does.
    This should help the reviewers give feedback faster and with higher
    quality. -->
    This PR adds a `OtlpOriginTagsResolver` in order to support getting the
    origin tags for OTLP Metrics.
    
    The tags come from values in `ResourceAttributes`, and the logic is
    copied from
    [here](https://github.com/DataDog/datadog-agent/blob/main/comp/otelcol/otlp/components/processor/infraattributesprocessor/common.go#L158).
    
    Note, only container id, pod id, process id are added here. There are
    some specific keys (i.e `AWSECSTaskARNKey`) that are missing in this PR
    since `RawOrigin` does not support that currently, with the dogstatsd
    source not using them.
    
    ## Change Type
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Non-functional (chore, refactoring, docs)
    - [ ] Performance
    
    
    ## How did you test this PR?
    <!-- Please how you tested these changes here -->
    
    Sent some OTLP metrics with resource attributes populated with
    `container_id` and made sure it showed up on datadog.
    
    ## References
    
    <!-- Please list any issues closed by this PR. -->
    
    <!--
    - Closes: <issue link>
    -->
    
    <!-- Any other issues or PRs relevant to this PR? Feel free to list them
    here. -->

---
+++ lib/saluki-components/src/sources/otlp/resolver.rs
@@ -0,0 +1,49 @@
+use std::{num::NonZeroUsize, sync::Arc, time::Duration};
+
+use saluki_context::{ContextResolver, ContextResolverBuilder, TagsResolverBuilder};
+use saluki_core::components::ComponentContext;
+use saluki_error::{generic_error, GenericError};
+use stringtheory::interning::GenericMapInterner;
+
+use crate::sources::{otlp::OtlpOriginTagResolver, OtlpConfiguration};
+
+const RESOLVER_CACHE_EXPIRATION: Duration = Duration::from_secs(30);
+
+/// Creates a new `ContextResolver` instance from the given configuration.
+///
+/// # Errors
+///
+/// If the context resolver string interner size is invalid, or there is an error creating either of the context
+/// resolvers, an error is returned.
+pub fn build_context_resolver(
+    config: &OtlpConfiguration, context: &ComponentContext, maybe_origin_tags_resolver: Option<OtlpOriginTagResolver>,
+) -> Result<ContextResolver, GenericError> {
+    let context_string_interner_size = NonZeroUsize::new(config.context_string_interner_bytes.as_u64() as usize)
+        .ok_or_else(|| generic_error!("context_string_interner_size must be greater than 0"))?;
+
+    let cached_contexts_limit = config.cached_contexts_limit;
+    let cached_tagsets_limit = config.cached_tagsets_limit;
+
+    let interner = GenericMapInterner::new(context_string_interner_size);
+
+    let tags_resolver = TagsResolverBuilder::new(format!("{}/otlp/tags", context.component_id()), interner.clone())?
+        .with_cached_tagsets_limit(cached_tagsets_limit)
+        .with_idle_tagsets_expiration(RESOLVER_CACHE_EXPIRATION)
+        .with_heap_allocations(config.allow_context_heap_allocations)
+        .with_origin_tags_resolver(
+            maybe_origin_tags_resolver
+                .map(|resolver| -> Arc<dyn saluki_context::origin::OriginTagsResolver> { Arc::new(resolver) }),
+        )
+        .build();
+
+    let resolver = ContextResolverBuilder::from_name(format!("{}/otlp/primary", context.component_id()))?
+        .with_interner_capacity_bytes(context_string_interner_size)
+        .with_cached_contexts_limit(cached_contexts_limit)
+        .with_idle_context_expiration(RESOLVER_CACHE_EXPIRATION)
+        .with_heap_allocations(config.allow_context_heap_allocations)
+        .with_tags_resolver(Some(tags_resolver.clone()))
+        .with_interner(interner.clone())
+        .build();
+
+    Ok(resolver)
+}
