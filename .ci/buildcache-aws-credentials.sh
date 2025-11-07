b5663a40fc69db97301fdefe5ce885d26380ec2a
Toby Lawrence <toby@nuclearfurnace.com>
2025-11-07 09:40:43 -0500

    source the AWS credentials in the same step as cargo build to ensure they're always valid

--- .ci/buildcache-aws-credentials.sh
+++ .ci/buildcache-aws-credentials.sh
@@ -2,20 +2,8 @@
 
 # Don't run the script if we're not in CI.
 if [ "${CI}" != "true" ]; then
-  echo "Not in CI. Skipping Buildcache configuration."
   exit 0
 fi
 
-# Create the configuration file path.
-mkdir -p ~/.buildcache
-
-# Get our credentials from IMDS and source them into the environment so we can write them out to the buildcache
-# configuration file.
-eval $(aws configure export-credentials --format env)
-
-cat <<EOF > ~/.buildcache/config.json
-{
-  "s3_access": "${AWS_ACCESS_KEY_ID}",
-  "s3_secret": "${AWS_SECRET_ACCESS_KEY}"
-}
-EOF
+# Get our credentials from IMDS and write them out so we can source them into the environment during the build step.
+aws configure export-credentials --format env
