3722e4f9ddf0ef23dc20988f9311929d17cd27df
Toby Lawrence <toby@nuclearfurnace.com>
2025-11-13 09:22:35 -0500

    chore(docs): fix bad doc comments in containerd-protos

--- lib/protos/containerd/build.rs
+++ lib/protos/containerd/build.rs
@@ -1,5 +1,6 @@
+use std::{fs, io, path::PathBuf};
+
 const CONTAINERD_PROTO_FILES: &[&str] = &[
-    // Types
     "proto/github.com/containerd/containerd/api/types/descriptor.proto",
     "proto/github.com/containerd/containerd/api/types/metrics.proto",
     "proto/github.com/containerd/containerd/api/types/mount.proto",
@@ -11,7 +12,6 @@ const CONTAINERD_PROTO_FILES: &[&str] = &[
     "proto/github.com/containerd/containerd/api/types/transfer/progress.proto",
     "proto/github.com/containerd/containerd/api/types/transfer/registry.proto",
     "proto/github.com/containerd/containerd/api/types/transfer/streaming.proto",
-    // Services
     "proto/github.com/containerd/containerd/api/services/containers/v1/containers.proto",
     "proto/github.com/containerd/containerd/api/services/content/v1/content.proto",
     "proto/github.com/containerd/containerd/api/services/diff/v1/diff.proto",
@@ -26,7 +26,6 @@ const CONTAINERD_PROTO_FILES: &[&str] = &[
     "proto/github.com/containerd/containerd/api/services/tasks/v1/tasks.proto",
     "proto/github.com/containerd/containerd/api/services/transfer/v1/transfer.proto",
     "proto/github.com/containerd/containerd/api/services/version/v1/version.proto",
-    // Events
     "proto/github.com/containerd/containerd/api/events/container.proto",
     "proto/github.com/containerd/containerd/api/events/content.proto",
     "proto/github.com/containerd/containerd/api/events/image.proto",
@@ -48,4 +47,37 @@ fn main() {
         .include_file("containerd.mod.rs")
         .compile_protos_with_config(config, CONTAINERD_PROTO_FILES, &["proto/"])
         .expect("failed to build gRPC service definitions for containerd");
+
+    if let Err(e) = fixup_bad_doc_comments() {
+        eprintln!("Failed to fixup bad doc comments: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn fixup_bad_doc_comments() -> Result<(), io::Error> {
+    // Some of the comments ported from the Protocol Buffers definitions end up getting parsed as Rust code
+    // in the doc comments, so we need to replace those usages with something that disables that.
+    let fixup_files = [
+        "containerd.services.containers.v1.rs",
+        "containerd.services.content.v1.rs",
+        "containerd.services.images.v1.rs",
+        "containerd.services.introspection.v1.rs",
+        "containerd.services.snapshots.v1.rs",
+    ];
+
+    let out_dir = std::env::var("OUT_DIR").map(PathBuf::from).unwrap();
+    for fixup_file in fixup_files {
+        let generated_file = out_dir.join(fixup_file);
+
+        let file_contents = fs::read_to_string(&generated_file)?.replace(
+            "/// 	filters\\[0\\] or filters\\[1\\] or ... or filters\\[n-1\\] or filters\\[n\\]",
+            r#"
+            /// ```text
+            /// 	filters[0] or filters[1] or ... or filters[n-1] or filters[n]
+            /// ```"#,
+        );
+        fs::write(&generated_file, file_contents)?;
+    }
+
+    Ok(())
 }
