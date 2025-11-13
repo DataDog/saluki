5445fb71c2635f5d6f9bbf6ad48b87ed1f6f418c
andrewqian2001datadog <andrew.qian@datadoghq.com>
2025-11-13 13:19:39 -0500

    move definition to top of file

--- lib/saluki-core/src/data_model/event/trace/mod.rs
+++ lib/saluki-core/src/data_model/event/trace/mod.rs
@@ -3,6 +3,32 @@
 use saluki_common::collections::FastHashMap;
 use stringtheory::MetaString;
 
+/// A trace event.
+///
+/// A trace is a collection of spans that represent a distributed trace.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Trace {
+    /// The spans that make up this trace.
+    spans: Vec<Span>,
+}
+
+impl Trace {
+    /// Creates a new `Trace` with the given spans.
+    pub fn new(spans: Vec<Span>) -> Self {
+        Self { spans }
+    }
+
+    /// Returns a reference to the spans in this trace.
+    pub fn spans(&self) -> &[Span] {
+        &self.spans
+    }
+
+    /// Returns a mutable reference to the spans in this trace.
+    pub fn spans_mut(&mut self) -> &mut Vec<Span> {
+        &mut self.spans
+    }
+}
+
 /// A span event.
 #[derive(Clone, Debug, PartialEq, Default)]
 pub struct Span {
@@ -395,30 +421,4 @@ pub enum AttributeScalarValue {
     Int(i64),
     /// Floating-point array value.
     Double(f64),
-}
-
-/// A trace event.
-///
-/// A trace is a collection of spans that represent a distributed trace.
-#[derive(Clone, Debug, PartialEq)]
-pub struct Trace {
-    /// The spans that make up this trace.
-    spans: Vec<Span>,
-}
-
-impl Trace {
-    /// Creates a new `Trace` with the given spans.
-    pub fn new(spans: Vec<Span>) -> Self {
-        Self { spans }
-    }
-
-    /// Returns a reference to the spans in this trace.
-    pub fn spans(&self) -> &[Span] {
-        &self.spans
-    }
-
-    /// Returns a mutable reference to the spans in this trace.
-    pub fn spans_mut(&mut self) -> &mut Vec<Span> {
-        &mut self.spans
-    }
 }
