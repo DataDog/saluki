c44659497125e360e45d50175ba22d807c171525
Toby Lawrence <toby@nuclearfurnace.com>
2025-11-19 21:51:37 -0500

    chore(ci): avoid DD_ADP_ENABLED to control ADP in Agent image

--- docker/cont-init.d/99-agent-data-plane.sh
+++ docker/cont-init.d/99-agent-data-plane.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [[ "${DD_ADP_ENABLED}" != "true" ]]; then
+if [[ "${DD_DATA_PLANE_ENABLED}" != "true" ]]; then
   exit 0
 fi
 
-# Enables the agent-data-plane s6 service.
-mkdir -p /etc/datadog-agent/adp
-touch /etc/datadog-agent/adp/enabled
-
-# Sets an environment variable override for the core agent to disable DSD.
+# Sets an environment variable override for the Core Agent to disable DSD.
 mkdir -p /run/agent/env
 printf "0" > /run/agent/env/DD_USE_DOGSTATSD
