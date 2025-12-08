894995e6ee34f804ba6ff420bbc68942d589ba07
Toby Lawrence <tobz@users.noreply.github.com>
2025-12-08 13:39:11 -0500

    chore(ci): update the CI build image to not use an ancient version of Ubuntu (#1045)

--- .ci/install-awscli.sh
+++ .ci/install-awscli.sh
@@ -3,5 +3,5 @@ set -o errexit -o verbose
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install --update
+./aws/install --update
 rm -rf ./aws
