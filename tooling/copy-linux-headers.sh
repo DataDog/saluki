36d315d3b54b3566d62e87fdd022a2569706774c
Toby Lawrence <toby@nuclearfurnace.com>
2025-12-11 00:01:33 -0500

    wip to build on ubuntu but static link via musl

---
+++ tooling/copy-linux-headers.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+#
+# Copies Linux headers from the host environment to the build target-specific include directory.
+#
+# This is necessary for providing the kernel headers to AWS-LC (and any other crate that requires them) when
+# cross-compiling <arch>-unknown-linux-musl.
+
+set -eux
+
+# Take the BUILD_TARGET environment variable and map it to the shortened target triple used
+# by `gcc` in terms of the specific include path we need to drop the headers in.
+#
+# We expect BUILD_TARGET to be set to the same target triple being passed to `rustc`.
+case "${BUILD_TARGET}" in
+    x86_64-unknown-linux-musl)
+        GCC_TARGET_TRIPLE=x86_64-linux-gnu
+        GCC_CROSS_TARGET_TRIPLE=x86_64-linux-musl
+        ;;
+    aarch64-unknown-linux-musl)
+        GCC_TARGET_TRIPLE=aarch64-linux-gnu
+        GCC_CROSS_TARGET_TRIPLE=aarch64-linux-musl
+        ;;
+    *)
+        echo "Unsupported target: ${BUILD_TARGET}"
+        exit 1
+        ;;
+esac
+
+# Make sure the kernel headers are installed at their expected location.
+if ! [ -d /usr/include/linux ] || ! [ -d /usr/include/asm-generic ] || ! [ -d /usr/include/${GCC_TARGET_TRIPLE} ]; then
+    echo "Kernel headers not found!"
+    exit 1
+fi
+
+# Copy over the kernel headers to the build target-specific include directory.
+cp -R /usr/include/linux /usr/include/${GCC_CROSS_TARGET_TRIPLE}/linux
+cp -R /usr/include/asm-generic /usr/include/${GCC_CROSS_TARGET_TRIPLE}/asm-generic
+cp -R /usr/include/${GCC_TARGET_TRIPLE}/asm /usr/include/${GCC_CROSS_TARGET_TRIPLE}/asm
