c617a89bb0a2128279d1e4b28150dac9bb8cfdb9
Toby Lawrence <toby@nuclearfurnace.com>
2026-01-09 11:36:47 -0500

    start wiring up a basic example of piecemeal for events + testing out the output

--- lib/saluki-components/src/encoders/datadog/events/mod.rs
+++ lib/saluki-components/src/encoders/datadog/events/mod.rs
@@ -266,3 +266,98 @@ fn encode_eventd(eventd: &EventD, tags_deduplicator: &mut ReusableDeduplicator<T
 
     event
 }
+
+fn encode_and_write_eventd_builder(
+    eventd: &EventD, tags_deduplicator: &mut ReusableDeduplicator<Tag>,
+) -> std::io::Result<Vec<u8>> {
+    let mut scratch_writer = piecemeal::ScratchWriter::new(Vec::new());
+
+    let mut payload_builder = datadog_protos::payload::builder::EventsPayloadBuilder::new(&mut scratch_writer);
+    payload_builder.add_events(|eb| {
+        eb.title(eventd.title())?.text(eventd.text())?;
+
+        if let Some(timestamp) = eventd.timestamp() {
+            eb.ts(timestamp as i64)?;
+        }
+
+        if let Some(priority) = eventd.priority() {
+            eb.priority(priority.as_str())?;
+        }
+
+        if let Some(alert_type) = eventd.alert_type() {
+            eb.alert_type(alert_type.as_str())?;
+        }
+
+        if let Some(hostname) = eventd.hostname() {
+            eb.host(hostname)?;
+        }
+
+        if let Some(aggregation_key) = eventd.aggregation_key() {
+            eb.aggregation_key(aggregation_key)?;
+        }
+
+        if let Some(source_type_name) = eventd.source_type_name() {
+            eb.source_type_name(source_type_name)?;
+        }
+
+        let chained_tags = eventd.tags().into_iter().chain(eventd.origin_tags());
+        let deduplicated_tags = tags_deduplicator.deduplicated(chained_tags);
+
+        eb.tags(|tb| tb.add_many_mapped(deduplicated_tags, |tag| tag.as_str()))?;
+
+        Ok(())
+    })?;
+
+    let mut output_buf = Vec::new();
+    scratch_writer.finish(&mut output_buf, false)?;
+
+    Ok(output_buf)
+}
+
+#[cfg(test)]
+mod tests {
+    use base64::Engine as _;
+    use saluki_context::tags::TagSet;
+    use saluki_core::data_model::event::eventd::Priority;
+    use stringtheory::MetaString;
+
+    use super::*;
+
+    #[test]
+    fn test_encode_and_write_eventd_builder() {
+        let eventd = EventD::new("title", "text");
+        let mut tags_deduplicator = ReusableDeduplicator::new();
+
+        let encoded = encode_and_write_eventd_builder(&eventd, &mut tags_deduplicator).unwrap();
+        assert!(!encoded.is_empty());
+    }
+
+    #[test]
+    fn test_encode_and_write_eventd_builder_vs_owned() {
+        let eventd = EventD::new("title", "text")
+            .with_hostname(MetaString::from_static("fake-host"))
+            .with_tags(TagSet::from_iter(vec![Tag::from("tag1"), Tag::from("tag2")]))
+            .with_priority(Priority::Normal);
+
+        let mut tags_deduplicator = ReusableDeduplicator::new();
+
+        let encoded_builder = encode_and_write_eventd_builder(&eventd, &mut tags_deduplicator).unwrap();
+        assert!(!encoded_builder.is_empty());
+
+        let mut encoded_owned = Vec::new();
+        encode_and_write_eventd(&eventd, &mut encoded_owned, &mut tags_deduplicator).unwrap();
+        assert!(!encoded_owned.is_empty());
+
+        // Print out both variants as base64:
+        println!(
+            "Encoded Builder: {}",
+            base64::engine::general_purpose::STANDARD.encode(&encoded_builder)
+        );
+        println!(
+            "Encoded Owned: {}",
+            base64::engine::general_purpose::STANDARD.encode(&encoded_owned)
+        );
+
+        assert_eq!(encoded_owned, encoded_builder);
+    }
+}
