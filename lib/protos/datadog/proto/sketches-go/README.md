b8e28d3eb200a7292cbadec349fffec5295c662f
raymond zhao <raymond.zhao@datadoghq.com>
2026-01-09 13:47:37 -0500

    Update make update-protos

---
+++ lib/protos/datadog/proto/sketches-go/README.md
@@ -0,0 +1,8 @@
+# DDSketch Protocol Buffers Definitions
+
+This directory contains Protocol Buffers definitions for DDSketch.
+
+## Source
+
+**Repository:** https://github.com/DataDog/sketches-go.git
+**Branch / Tag**: v1.4.7
--- tooling/update-protos.sh
+++ tooling/update-protos.sh
@@ -8,14 +8,16 @@ set -eu
 # - DD_AGENT_GIT_TAG: Git tag for the `DataDog/datadog-agent` repository to pull Protocol Buffers definitions from.
 # - AGENT_PAYLOAD_GIT_TAG: Git tag for the `DataDog/agent-payload` repository to pull Protocol Buffers definitions from.
 # - CONTAINERD_GIT_TAG: Git tag for the `containerd/containerd` repository to pull Protocol Buffers definitions from.
+# - SKETCHES_GO_GIT_TAG: Git tag for the `DataDog/sketches-go` repository to pull Protocol Buffers definitions from.
 
 DD_AGENT_GIT_URL="https://github.com/DataDog/datadog-agent.git"
 AGENT_PAYLOAD_GIT_URL="https://github.com/DataDog/agent-payload.git"
 CONTAINERD_GIT_URL="https://github.com/containerd/containerd.git"
+SKETCHES_GO_GIT_URL="https://github.com/DataDog/sketches-go.git"
 
 # Ensure that the required environment variables are set
-if [ -z "${DD_AGENT_GIT_TAG:-}" ] || [ -z "${AGENT_PAYLOAD_GIT_TAG:-}" ] || [ -z "${CONTAINERD_GIT_TAG:-}" ]; then
-    echo "[!]: DD_AGENT_GIT_TAG, AGENT_PAYLOAD_GIT_TAG, and CONTAINERD_GIT_TAG environment variables must be set. Exiting."
+if [ -z "${DD_AGENT_GIT_TAG:-}" ] || [ -z "${AGENT_PAYLOAD_GIT_TAG:-}" ] || [ -z "${CONTAINERD_GIT_TAG:-}" ] || [ -z "${SKETCHES_GO_GIT_TAG:-}" ]; then
+    echo "[!]: DD_AGENT_GIT_TAG, AGENT_PAYLOAD_GIT_TAG, CONTAINERD_GIT_TAG, and SKETCHES_GO_GIT_TAG environment variables must be set. Exiting."
     exit 1
 fi
 
@@ -31,6 +33,9 @@ git -C ${TMP_DIR} -c advice.detachedHead=false clone --quiet --depth 1 --branch 
 
 echo "[*] Checking out the containerd repository (${CONTAINERD_GIT_TAG})..."
 git -C ${TMP_DIR} -c advice.detachedHead=false clone --quiet --depth 1 --branch ${CONTAINERD_GIT_TAG} ${CONTAINERD_GIT_URL}
+
+echo "[*] Checking out the sketches-go repository (${SKETCHES_GO_GIT_TAG})..."
+git -C ${TMP_DIR} -c advice.detachedHead=false clone --quiet --depth 1 --branch ${SKETCHES_GO_GIT_TAG} ${SKETCHES_GO_GIT_URL}
 
 # Copy the relevant Protocol Buffers definitions to the appropriate locations.
 echo "[*] Copying Protocol Buffers definitions to lib/protos/*..."
@@ -50,6 +55,10 @@ mkdir -p lib/protos/containerd/proto/github.com/containerd/containerd/
 cp -R ${TMP_DIR}/containerd/api lib/protos/containerd/proto/github.com/containerd/containerd/
 
 find lib/protos/containerd/proto/ -type f  ! -name "*.proto" -delete
+
+## sketches-go DDSketch definitions.
+mkdir -p lib/protos/datadog/proto/sketches-go/ddsketch/pb
+cp ${TMP_DIR}/sketches-go/ddsketch/pb/ddsketch.proto lib/protos/datadog/proto/sketches-go/ddsketch/pb/
 
 # Update the README.md files to specify what version the definitions are based on.
 cat <<EOF > lib/protos/datadog/proto/datadog-agent/README.md
@@ -84,6 +93,17 @@ This directory contains Protocol Buffers definitions for containerd.
 
 **Repository:** ${CONTAINERD_GIT_URL}
 **Branch / Tag**: ${CONTAINERD_GIT_TAG}
+EOF
+
+cat <<EOF > lib/protos/datadog/proto/sketches-go/README.md
+# DDSketch Protocol Buffers Definitions
+
+This directory contains Protocol Buffers definitions for DDSketch.
+
+## Source
+
+**Repository:** ${SKETCHES_GO_GIT_URL}
+**Branch / Tag**: ${SKETCHES_GO_GIT_TAG}
 EOF
 
 # Clean up the temporary directory.
