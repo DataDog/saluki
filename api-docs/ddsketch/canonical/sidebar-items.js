368ca07db86ad6b1538e040d72cf829bd8a6d877
tobz <tobz@users.noreply.github.com>
2026-01-15 22:05:59 +0000

    enhancement(ddsketch): add canonical implementation of DDSketch (#1104) 68385ad87cb92f13be13d87e7dd45310e4620fc7

---
+++ api-docs/ddsketch/canonical/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ProtoConversionError"],"mod":["mapping","store"],"struct":["DDSketch"]};
---
+++ api-docs/ddsketch/canonical/store/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["CollapsingHighestDenseStore","CollapsingLowestDenseStore","DenseStore","SparseStore"],"trait":["Store"]};
