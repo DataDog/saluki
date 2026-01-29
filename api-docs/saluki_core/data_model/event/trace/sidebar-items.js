84a6fee4534045a4df9898a11027a9ba12ca9daa
tobz <tobz@users.noreply.github.com>
2026-01-29 13:59:16 +0000

    [AGNTLOG-468] chore(datadog): add trace sampling  (#1110) e33b714b60f4146ca3c73325bfea0e967c042547

--- api-docs/saluki_core/data_model/event/trace/sidebar-items.js
+++ api-docs/saluki_core/data_model/event/trace/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["AttributeScalarValue","AttributeValue"],"struct":["Span","SpanEvent","SpanLink","Trace"]};
+window.SIDEBAR_ITEMS = {"enum":["AttributeScalarValue","AttributeValue"],"struct":["Span","SpanEvent","SpanLink","Trace","TraceSampling"]};
