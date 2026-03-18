e30e9a5bc39df0752571eea4e649065aac711831
Toby Lawrence <toby@nuclearfurnace.com>
2026-03-18 06:00:05 -0400

    wip

--- lib/saluki-components/src/sources/checks_ipc/mod.rs
+++ lib/saluki-components/src/sources/checks_ipc/mod.rs
@@ -1,21 +1,29 @@
 use std::sync::LazyLock;
+use std::time::Duration;
 
 use async_trait::async_trait;
+use datadog_protos::checks::check_data::Data;
 use datadog_protos::checks::checks_server::{Checks, ChecksServer};
 use datadog_protos::checks::{SendCheckPayloadRequest, SendCheckPayloadResponse};
+use datadog_protos::metrics::MetricType;
 use memory_accounting::{MemoryBounds, MemoryBoundsBuilder};
+use protobuf::Enum as _;
 use saluki_common::task::HandleExt as _;
 use saluki_config::GenericConfiguration;
+use saluki_context::tags::{Tag, TagSet};
+use saluki_context::Context;
 use saluki_core::components::{sources::*, ComponentContext};
-use saluki_core::data_model::event::EventType;
+use saluki_core::data_model::event::metric::Metric;
+use saluki_core::data_model::event::{Event, EventType};
 use saluki_core::topology::OutputDefinition;
-use saluki_error::{generic_error, GenericError};
+use saluki_error::{generic_error, ErrorContext as _, GenericError};
 use saluki_io::net::ListenAddress;
 use serde::Deserialize;
 use tokio::select;
+use tokio::sync::mpsc;
 use tonic::transport::Server;
 use tonic::{Response, Status};
-use tracing::{debug, info};
+use tracing::{debug, info, warn};
 
 const fn default_grpc_endpoint() -> ListenAddress {
     ListenAddress::any_tcp(5105)
@@ -68,7 +76,9 @@ impl Source for ChecksIPC {
         let mut global_shutdown = context.take_shutdown_handle();
         let mut health = context.take_health_handle();
 
-        let grpc_server = Server::builder().add_service(ChecksServer::new(ChecksService));
+        let (events_tx, mut events_rx) = mpsc::channel(16);
+
+        let grpc_server = Server::builder().add_service(ChecksServer::new(ChecksService { events_tx }));
 
         let grpc_socket_addr = match self.grpc_endpoint {
             ListenAddress::Tcp(addr) => addr,
@@ -89,6 +99,16 @@ impl Source for ChecksIPC {
                     break;
                 },
                 _ = health.live() => continue,
+                Some(event) = events_rx.recv() => match event {
+                    Event::Metric(metric) => {
+                        let buffered = context.dispatcher().buffered_named("metrics")
+                            .error_context("Failed to get buffered dispatcher for metrics")?;
+                        if let Err(e) = buffered.send_all([Event::Metric(metric)]).await {
+                            warn!("Failed to dispatch metric event: {:?}", e);
+                        }
+                    },
+                    _ => {},
+                },
             }
         }
 
@@ -97,12 +117,14 @@ impl Source for ChecksIPC {
     }
 }
 
-struct ChecksService;
+struct ChecksService {
+    events_tx: mpsc::Sender<Event>,
+}
 
 #[async_trait]
 impl Checks for ChecksService {
     async fn send_check_payload(
-        &self, _request: tonic::Request<SendCheckPayloadRequest>,
+        &self, request: tonic::Request<SendCheckPayloadRequest>,
     ) -> Result<Response<SendCheckPayloadResponse>, Status> {
         // command for testing locally:
         //
@@ -110,6 +132,54 @@ impl Checks for ChecksService {
         // grpcurl -plaintext -proto lib/protos/datadog/proto/checks/checks.proto localhost:5105 datadog.checks.Checks/SendCheckPayload
 
         info!("Received check payload.");
+
+        let payload = request
+            .into_inner()
+            .payload
+            .ok_or_else(|| Status::invalid_argument("payload is required"))?;
+        for check_data in payload.data.into_iter().filter_map(|data| data.data) {
+            let event = match check_data {
+                Data::Metric(metric) => {
+                    let metric_type = match MetricType::from_i32(metric.r#type) {
+                        Some(typ) => typ,
+                        None => continue,
+                    };
+
+                    let tags = metric.tags.into_iter().map(Tag::from).collect::<TagSet>();
+                    let context = Context::from_parts(metric.name, tags.into_shared());
+                    let points = metric
+                        .points
+                        .into_iter()
+                        .map(|point| (point.timestamp as u64, point.value))
+                        .collect::<Vec<_>>();
+                    let metric = match metric_type {
+                        MetricType::COUNT => Metric::counter(context, &points[..]),
+                        MetricType::GAUGE => Metric::gauge(context, &points[..]),
+                        MetricType::RATE => {
+                            let interval_secs = metric.interval_secs as u64;
+                            if interval_secs == 0 {
+                                warn!("Received rate metric from check with interval of zero. Skipping.");
+                                continue;
+                            }
+
+                            Metric::rate(context, &points[..], Duration::from_secs(interval_secs))
+                        }
+                        MetricType::UNSPECIFIED => {
+                            warn!("Received metric with unspecified type. Skipping.");
+                            continue;
+                        }
+                    };
+
+                    Event::Metric(metric)
+                }
+                _ => continue,
+            };
+
+            if let Err(e) = self.events_tx.send(event).await {
+                warn!("Failed to send metric event: {:?}", e);
+            }
+        }
+
         Ok(Response::new(SendCheckPayloadResponse {}))
     }
 }
