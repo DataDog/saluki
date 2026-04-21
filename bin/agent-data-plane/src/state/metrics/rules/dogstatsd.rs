e281aa54f7578b8fec47178e2556ae8a421ecdbf
andrewqian2001datadog <andrew.qian@datadoghq.com>
2026-04-21 14:41:57 -0400

    remap ADP metrics to datadog agent naming convention

--- bin/agent-data-plane/src/state/metrics/rules/dogstatsd.rs
+++ bin/agent-data-plane/src/state/metrics/rules/dogstatsd.rs
@@ -4,6 +4,21 @@ pub fn get_dogstatsd_remappings() -> Vec<RemapperRule> {
     vec![
         // DogStatsD metrics.
         RemapperRule::by_name_and_tags(
+            "adp.metric_filterlist_size",
+            &["component_id:dsd_prefix_filter"],
+            "datadog.agent.filterlist.size",
+        ),
+        RemapperRule::by_name_and_tags(
+            "adp.metric_filterlist_updates_total",
+            &["component_id:dsd_prefix_filter"],
+            "datadog.agent.filterlist.updates",
+        ),
+        RemapperRule::by_name_and_tags(
+            "adp.dogstatsd_listener_filtered_points_total",
+            &["component_id:dsd_prefix_filter"],
+            "datadog.agent.dogstatsd.listener_filtered_points",
+        ),
+        RemapperRule::by_name_and_tags(
             "adp.object_pool_acquired",
             &["pool_name:dsd_packet_bufs"],
             "dogstatsd.packet_pool_get",
