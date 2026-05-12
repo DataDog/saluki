a1559f4313b04e0f46f12230f3a40f8694fe66cd
tobz <tobz@users.noreply.github.com>
2026-05-12 14:46:05 +0000

    enhancement(dogstatsd): allow autoscaling UDP stream handlers on available parallelism (#1624) 285fa250372c43cac7e1714d64f0f143480722bc

--- api-docs/saluki_io/net/unix/sidebar-items.js
+++ api-docs/saluki_io/net/unix/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["enable_uds_socket_credentials","unix_recvmsg","unixgram_recvmsg"]};
+window.SIDEBAR_ITEMS = {"fn":["enable_uds_socket_credentials","socket_reuseport_supported","unix_recvmsg","unixgram_recvmsg"]};
--- api-docs/search.index/17932a9547.js
+++
@@ -1,1 +0,0 @@
-rn_("CQAbA3H34qe2xAAAbiMAYXU=")
