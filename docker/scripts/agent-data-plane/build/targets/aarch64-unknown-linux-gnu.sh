49fe0e5aac8b5923a7206654133d0ec485007300
Toby Lawrence <toby@nuclearfurnace.com>
2026-07-14 11:37:34 -0400

    assert on glibc 2.17 for both amd64 and arm64

--- docker/scripts/agent-data-plane/build/targets/aarch64-unknown-linux-gnu.sh
+++ docker/scripts/agent-data-plane/build/targets/aarch64-unknown-linux-gnu.sh
@@ -13,7 +13,7 @@ if [ -d /opt/toolchains/aarch64 ]; then
     export CC_aarch64_unknown_linux_gnu="${_ctng}-gcc"
     export CXX_aarch64_unknown_linux_gnu="${_ctng}-g++"
     export AR_aarch64_unknown_linux_gnu="${_ctng}-ar"
-    TARGET_MAX_GLIBC="2.23"
+    TARGET_MAX_GLIBC="2.17"
 fi
 
 TARGET_CARGO_ARGS="--target aarch64-unknown-linux-gnu"
