e9127423452e8ea631d1e3c6181047fc787694ce
Ilya Rubnich <ilya.rubnich@datadoghq.com>
2026-08-18 14:07:09 -0400

    feat(datadog-agent-commons): add minimal IPC TLS feature

--- lib/datadog-agent/commons/src/ipc/mod.rs
+++ lib/datadog-agent/commons/src/ipc/mod.rs
@@ -1,6 +1,9 @@
 //! IPC primitives for interacting with the Datadog Agent.
 
+#[cfg(feature = "full")]
 pub mod client;
+#[cfg(feature = "full")]
 pub mod config;
+#[cfg(feature = "full")]
 pub mod session;
 pub mod tls;
